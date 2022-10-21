3250266a90d8d1910a1a2e4ee92ef43060bf7232
LucasLvy <lucaslevy10@gmail.com>
2022-10-22 00:30:51 +0200

    feat(block + arithmetic): python for them

--- tests/real_tests/units/test_arithmetic_operations.py
+++ tests/real_tests/units/test_arithmetic_operations.py
@@ -10,7 +10,7 @@ class TestBasic(IsolatedAsyncioTestCase):
         async def _setUpClass(cls) -> None:
             cls.starknet = await Starknet.empty()
             cls.unit_test = await cls.starknet.deploy(
-                source="./tests/units/kakarot/instructions/test_arithmetic_operations.cairo",
+                source="./tests/real_tests/cairo_files/instructions/test_arithmetic_operations.cairo",
                 cairo_path=["src"],
                 disable_hint_validation=True,
             )
@@ -22,13 +22,13 @@ class TestBasic(IsolatedAsyncioTestCase):
         cairo_coverage.report_runs(excluded_file={"site-packages"})
 
     async def test_everything(self):
-        await self.unit_test.test__add__should_add_0_and_1().call()
-        await self.unit_test.test__mul__should_mul_0_and_1().call()
-        await self.unit_test.test__sub__should_sub_0_and_1().call()
-        await self.unit_test.test__div__should_div_0_and_1().call()
-        await self.unit_test.test__sdiv__should_signed_div_0_and_1().call()
-        await self.unit_test.test__mod__should_mod_0_and_1().call()
-        await self.unit_test.test__smod__should_smod_0_and_1().call()
-        await self.unit_test.test__addmod__should_add_0_and_1_and_div_rem_by_2().call()
-        await self.unit_test.test__mulmod__should_mul_0_and_1_and_div_rem_by_2().call()
-        await self.unit_test.test__signextend__should_signextend_0_and_1().call()
+        await self.unit_test.test__exec_add__should_add_0_and_1().call()
+        await self.unit_test.test__exec_mul__should_mul_0_and_1().call()
+        await self.unit_test.test__exec_sub__should_sub_0_and_1().call()
+        await self.unit_test.test__exec_div__should_div_0_and_1().call()
+        await self.unit_test.test__exec_sdiv__should_signed_div_0_and_1().call()
+        await self.unit_test.test__exec_mod__should_mod_0_and_1().call()
+        await self.unit_test.test__exec_smod__should_smod_0_and_1().call()
+        await self.unit_test.test__exec_addmod__should_add_0_and_1_and_div_rem_by_2().call()
+        await self.unit_test.test__exec_mulmod__should_mul_0_and_1_and_div_rem_by_2().call()
+        await self.unit_test.test__exec_exp__should_exp_0_and_1().call()
---
+++ tests/real_tests/units/test_block_information.py
@@ -0,0 +1,25 @@
+from unittest import IsolatedAsyncioTestCase
+from asyncio import run
+from starkware.starknet.testing.starknet import Starknet
+from cairo_coverage import cairo_coverage
+
+
+class TestBasic(IsolatedAsyncioTestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        async def _setUpClass(cls) -> None:
+            cls.starknet = await Starknet.empty()
+            cls.unit_test = await cls.starknet.deploy(
+                source="./tests/real_tests/cairo_files/instructions/test_block_information.cairo",
+                cairo_path=["src"],
+                disable_hint_validation=True,
+            )
+
+        run(_setUpClass(cls))
+
+    @classmethod
+    def tearDownClass(cls):
+        cairo_coverage.report_runs(excluded_file={"site-packages"})
+
+    async def test_everything(self):
+        await self.unit_test.test__chainId__should_add_0_and_1().call()
