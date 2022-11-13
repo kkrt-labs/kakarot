09350a87ae75b252e4ec7f779316063830531a3a
Clément Walter <clement0walter@gmail.com>
2022-11-13 15:46:25 +0100

    Add timeit and traceit decorators for optimization purposes (#237)
    ## Pull request type
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [x] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## What is the current behavior?
    
    Some `print` spread apart in tests to have resources and time consumed.
    
    ## What is the new behavior?
    
    Automatic reporting of any call to `deploy`, `declare` or `zk_evm.<>`.
    
    ## Other information
    
    Not sure how we want to store/display the results. Open to discussion.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
 import asyncio
-from time import time
+from pathlib import Path
 from typing import AsyncGenerator
 
+import pandas as pd
 import pytest
 import pytest_asyncio
 from cairo_coverage import cairo_coverage
 from starkware.starknet.business_logic.state.state_api_objects import BlockInfo
 from starkware.starknet.testing.starknet import Starknet
+
+from tests.utils.utils import dump_reports, reports, timeit
+
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 500)
 
 
 @pytest.fixture(scope="session")
@@ -22,7 +28,11 @@ async def starknet() -> AsyncGenerator[Starknet, None]:
     starknet.state.state.update_block_info(
         BlockInfo.create_for_testing(block_number=1, block_timestamp=1)
     )
+    starknet.deploy = timeit(starknet.deploy)
+    starknet.declare = timeit(starknet.declare)
+
     yield starknet
+
     files = cairo_coverage.report_runs(excluded_file={"site-packages", "cairo_files"})
     total_covered = []
     for file in files:
@@ -31,6 +41,22 @@ async def starknet() -> AsyncGenerator[Starknet, None]:
         total_covered.append(file.pct_covered)
     if files and (val := not sum(total_covered) / len(files)) >= 80:
         print(f"WARNING: Project is not covered enough {val:.2f})")
+
+    dump_reports("coverage")
+    times, resources = reports()
+    print(
+        times.assign(
+            contract=lambda df: df.kwargs.map(lambda kw: Path(kw["source"]).stem)
+        )
+        .filter(items=["name", "contract", "duration"])
+        .sort_values("duration", ascending=False)
+    )
+    print(
+        resources.groupby("context")
+        .agg("sum", numeric_only=True)
+        .sort_values("n_steps", ascending=False)
+        .astype(int)
+    )
 
 
 @pytest_asyncio.fixture(scope="session")
@@ -44,17 +70,12 @@ async def eth(starknet):
 
 @pytest_asyncio.fixture(scope="session")
 async def account_registry(starknet: Starknet):
-    print("account_registry")
-    start = time()
-    registry = await starknet.deploy(
+    return await starknet.deploy(
         source="./src/kakarot/accounts/registry/account_registry.cairo",
         cairo_path=["src"],
         disable_hint_validation=False,
         constructor_calldata=[1],
     )
-    registry_time = time()
-    print(f"AccountRegistry deployed in {registry_time - start:.2f}s")
-    return registry
 
 
 @pytest_asyncio.fixture(scope="session")
---
+++ tests/utils/utils.py
@@ -0,0 +1,159 @@
+import json
+import logging
+from contextlib import contextmanager
+from pathlib import Path
+from time import perf_counter
+from typing import Callable, Union
+
+import pandas as pd
+from starkware.starknet.testing.contract import StarknetContractFunctionInvocation
+from starkware.starknet.testing.starknet import StarknetContract
+
+logging.basicConfig(format="%(levelname)-8s %(message)s")
+logger = logging.getLogger("timer")
+
+
+_time_report = []
+_resources_report = []
+
+
+def timeit(fun):
+    async def timed_fun(*args, **kwargs):
+        start = perf_counter()
+        res = await fun(*args, **kwargs)
+        stop = perf_counter()
+        duration = stop - start
+        _time_report.append(
+            {"name": fun.__name__, "args": args, "kwargs": kwargs, "duration": duration}
+        )
+        logger.info(
+            f"{fun.__name__}({json.dumps(args)}, {json.dumps(kwargs)}) in {duration:.2f}s"
+        )
+        return res
+
+    return timed_fun
+
+
+class traceit:
+    """
+    Record resources used by a StarknetContract
+    """
+
+    _context = ""
+
+    @classmethod
+    @property
+    def prefix(cls):
+        """
+        Prefix the log output with the context if used
+        """
+        return cls._context + ":" if cls._context != "" else ""
+
+    @classmethod
+    def _trace_call(
+        cls,
+        invoke_fun: Callable,
+        contract_name: str,
+        attr_name: str,
+        *args,
+        **kwargs,
+    ):
+        """
+        StarknetContract instances have methods defined in their corresponding cairo file.
+        These methods, once called, return a StarknetContractFunctionInvocation instance.
+        This class defines "call" and "execute" methods for view or external call to the underlying functions.
+        The invoke_fun here is either one or the other.
+        This wrapper will record the ExecutionResource of the corresponding call.
+        """
+
+        async def traced_fun(*a, **kw):
+            res = await invoke_fun(*a, **kw)
+            resources = res.call_info.execution_resources.Schema().dump(
+                res.call_info.execution_resources
+            )
+            resources = {
+                **resources.pop("builtin_instance_counter"),
+                **resources,
+            }
+            _resources_report.append(
+                {
+                    **({"context": cls._context} if cls._context != "" else {}),
+                    "contract_name": contract_name,
+                    "function_name": attr_name,
+                    "args": args,
+                    "kwargs": kwargs,
+                    **resources,
+                }
+            )
+            logger.info(
+                f"{cls.prefix}{contract_name}.{attr_name}({json.dumps(args)}, {json.dumps(kwargs)}) used {resources}"
+            )
+            return res
+
+        return traced_fun
+
+    @classmethod
+    def _trace_attr(cls, fun: Callable, contract_name: str):
+        """
+        Wrapper to be applied to a contract's method to keep track of the ExecutionResources used.
+        """
+
+        def wrapped(*args, **kwargs):
+            prepared_call = fun(*args, **kwargs)
+            for invoke_fun_name in ["execute", "call"]:
+                if hasattr(prepared_call, invoke_fun_name):
+                    setattr(
+                        prepared_call,
+                        invoke_fun_name,
+                        cls._trace_call(
+                            getattr(prepared_call, invoke_fun_name),
+                            contract_name,
+                            fun.__name__,
+                            *args,
+                            **kwargs,
+                        ),
+                    )
+            return prepared_call
+
+        return wrapped
+
+    @staticmethod
+    def trace(contract: StarknetContract, name: str) -> StarknetContract:
+        for attr_name in contract._abi_function_mapping.keys():
+            setattr(
+                contract,
+                attr_name,
+                traceit._trace_attr(
+                    getattr(contract, attr_name),
+                    contract_name=name,
+                ),
+            )
+        return contract
+
+    @classmethod
+    @contextmanager
+    def context(
+        cls,
+        context,
+    ):
+        """
+        Context manager to add a context field to the logs and records
+        """
+        prev_context = cls._context
+        cls._context = context
+        yield
+        cls._context = prev_context
+
+
+def reports():
+    return pd.DataFrame(_time_report), pd.DataFrame(_resources_report).sort_values(
+        ["n_steps"], ascending=False
+    ).fillna({"context": ""}).fillna(0)
+
+
+def dump_reports(path: Union[str, Path]):
+    p = Path(path)
+    p.mkdir(exist_ok=True, parents=True)
+    times, traces = reports()
+    times.to_csv(p / "times.csv")
+    traces.to_csv(p / "resources.csv")
