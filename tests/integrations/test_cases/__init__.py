9e974d35c78e8745c08b9e80a4e9b2b037c15bf3
Clément Walter <clement0walter@gmail.com>
2022-11-18 13:58:15 +0100

    Add solidity_contract factory fixture for working with any .sol file

--- tests/integrations/test_cases/__init__.py
+++ tests/integrations/test_cases/__init__.py
@@ -1,12 +1,7 @@
 import pytest
 
 from .execute import test_cases as test_cases_execute
-from .execute_at_address import test_cases as test_cases_execute_at_address
 
 params_execute = [
     pytest.param(case.pop("params"), **case) for case in test_cases_execute
-]
-
-params_execute_at_address = [
-    pytest.param(case.pop("params"), **case) for case in test_cases_execute_at_address
 ]
--- tests/integrations/test_zk_evm.py
+++ tests/integrations/test_zk_evm.py
@@ -1,48 +1,10 @@
+from typing import Callable
+
 import pytest
-import pytest_asyncio
-from starkware.starknet.testing.contract import DeclaredClass, StarknetContract
-from starkware.starknet.testing.starknet import Starknet
+from starkware.starknet.testing.contract import StarknetContract
 
-from tests.integrations.test_cases import params_execute, params_execute_at_address
-from tests.utils.utils import (
-    get_contract,
-    hex_string_to_bytes_array,
-    int_to_uint256,
-    traceit,
-    wrap_for_kakarot,
-)
-
-
-@pytest_asyncio.fixture(scope="module")
-async def zk_evm(
-    starknet: Starknet, eth: StarknetContract, contract_account_class: DeclaredClass
-) -> StarknetContract:
-    return await starknet.deploy(
-        source="./src/kakarot/kakarot.cairo",
-        cairo_path=["src"],
-        disable_hint_validation=False,
-        constructor_calldata=[
-            1,
-            eth.contract_address,
-            contract_account_class.class_hash,
-        ],
-    )
-
-
-@pytest_asyncio.fixture(scope="module", autouse=True)
-async def set_account_registry(
-    zk_evm: StarknetContract, account_registry: StarknetContract
-):
-    await account_registry.transfer_ownership(zk_evm.contract_address).execute(
-        caller_address=1
-    )
-    await zk_evm.set_account_registry(
-        registry_address_=account_registry.contract_address
-    ).execute(caller_address=1)
-    yield
-    await account_registry.transfer_ownership(1).execute(
-        caller_address=zk_evm.contract_address
-    )
+from tests.integrations.test_cases import params_execute
+from tests.utils.utils import hex_string_to_bytes_array, int_to_uint256, traceit
 
 
 @pytest.mark.asyncio
@@ -51,15 +13,15 @@ class TestZkEVM:
         "params",
         params_execute,
     )
-    async def test_execute(self, zk_evm: StarknetContract, params: dict, request):
+    async def test_execute(self, kakarot: StarknetContract, params: dict, request):
         with traceit.context(request.node.callspec.id):
-            res = await zk_evm.execute(
+            res = await kakarot.execute(
                 value=int(params["value"]),
                 bytecode=hex_string_to_bytes_array(params["code"]),
                 calldata=hex_string_to_bytes_array(params["calldata"]),
             ).call(caller_address=1)
 
-        Uint256 = zk_evm.struct_manager.get_contract_struct("Uint256")
+        Uint256 = kakarot.struct_manager.get_contract_struct("Uint256")
         assert res.result.stack == [
             Uint256(*int_to_uint256(int(s)))
             for s in (params["stack"].split(",") if params["stack"] else [])
@@ -76,103 +38,55 @@ class TestZkEVM:
                 for event in sorted(res.call_info.events, key=lambda x: x.order)
             ] == events
 
-    @pytest_asyncio.fixture(scope="module")
-    async def erc_20(self, zk_evm: StarknetContract) -> dict:
-        ERC20 = get_contract("ERC20")
-        deploy_bytecode = hex_string_to_bytes_array(
-            ERC20.constructor("name", "symbol", 18).data_in_transaction
-        )
-
-        with traceit.context("deploy kakarot erc20"):
-            tx = await zk_evm.deploy(bytecode=deploy_bytecode).execute(caller_address=1)
-
-        return {
-            "contract": wrap_for_kakarot(ERC20, zk_evm, tx.result.evm_contract_address),
-            "tx": tx,
-        }
-
     @pytest.mark.skip(
         "One byte is different, should investigate after resolving the other skipped tests"
     )
     async def test_deploy(
         self,
-        starknet: Starknet,
-        erc_20: dict,
-        contract_account_class: DeclaredClass,
+        solidity_contract: Callable,
     ):
-        starknet_contract_address = erc_20["tx"].result.starknet_contract_address
-        contract_account = StarknetContract(
-            starknet.state,
-            contract_account_class.abi,
-            starknet_contract_address,
-            erc_20["tx"],
-        )
-        stored_bytecode = (await contract_account.bytecode().call()).result.bytecode
-        contract_bytecode = hex_string_to_bytes_array(erc_20["contract"].bytecode.hex())
+        erc_20 = await solidity_contract("ERC20", "name", "symbol", 18)
+        stored_bytecode = (
+            await erc_20.contract_account.bytecode().call()
+        ).result.bytecode
+        contract_bytecode = hex_string_to_bytes_array(erc_20.bytecode.hex())
         deployed_bytecode = contract_bytecode[contract_bytecode.index(0xFE) + 1 :]
         assert stored_bytecode == deployed_bytecode
-        name = await erc_20["contract"].name()
+        name = await erc_20.name()
         assert name == "name"
-        symbol = await erc_20["contract"].symbol()
+        symbol = await erc_20.symbol()
         assert symbol == "symbol"
-        decimals = await erc_20["contract"].decimals()
+        decimals = await erc_20.decimals()
         assert decimals == 18
 
-    @pytest.mark.parametrize(
-        "params",
-        params_execute_at_address,
-    )
-    async def test_execute_at_address(
-        self,
-        zk_evm: StarknetContract,
-        erc_20: dict,
-        params: dict,
-        request,
-    ):
-        state = zk_evm.state.copy()
-        with traceit.context(request.node.callspec.id):
-            res = await zk_evm.execute_at_address(
-                address=erc_20["tx"].result.evm_contract_address,
-                value=params["value"],
-                calldata=hex_string_to_bytes_array(params["calldata"]),
-            ).execute(caller_address=2)
-
-        assert res.result.return_data == hex_string_to_bytes_array(
-            params["return_value"]
-        )
-        zk_evm.state = state
-
     @pytest.mark.SolmateERC20
-    async def test_erc20(self, zk_evm: StarknetContract, erc_20: dict, request):
-        state = zk_evm.state.copy()
+    async def test_erc20(
+        self, kakarot: StarknetContract, solidity_contract: Callable, request
+    ):
+        state = kakarot.state.copy()
+        erc_20 = await solidity_contract("ERC20", "name", "symbol", 18)
         caller_addresses = list(range(4))
         addresses = ["0x" + "0" * 39 + str(i) for i in caller_addresses]
         with traceit.context(request.node.own_markers[0].name):
 
-            await erc_20["contract"].mint(
-                addresses[2], 0x164, caller_address=caller_addresses[1]
-            )
+            await erc_20.mint(addresses[2], 0x164, caller_address=caller_addresses[1])
 
-            total_supply = await erc_20["contract"].totalSupply()
+            total_supply = await erc_20.totalSupply()
             assert total_supply == 0x164
 
-            await erc_20["contract"].approve(
+            await erc_20.approve(
                 addresses[1], 0xF4240, caller_address=caller_addresses[2]
             )
 
-            allowance = await erc_20["contract"].allowance(addresses[2], addresses[1])
+            allowance = await erc_20.allowance(addresses[2], addresses[1])
             assert allowance == 0xF4240
 
-            balances_before = [
-                await erc_20["contract"].balanceOf(address) for address in addresses
-            ]
+            balances_before = [await erc_20.balanceOf(address) for address in addresses]
 
-            await erc_20["contract"].transferFrom(
+            await erc_20.transferFrom(
                 addresses[2], addresses[1], 0xA, caller_address=caller_addresses[1]
             )
-            balances_after = [
-                await erc_20["contract"].balanceOf(address) for address in addresses
-            ]
+            balances_after = [await erc_20.balanceOf(address) for address in addresses]
 
             assert balances_after[0] - balances_before[0] == 0
             assert balances_after[1] - balances_before[1] == 0xA
@@ -181,16 +95,11 @@ class TestZkEVM:
 
             balances_before = balances_after
 
-            await erc_20["contract"].transfer(
-                addresses[3], 0x5, caller_address=caller_addresses[1]
-            )
-            balances_after = [
-                await erc_20["contract"].balanceOf(address) for address in addresses
-            ]
+            await erc_20.transfer(addresses[3], 0x5, caller_address=caller_addresses[1])
+            balances_after = [await erc_20.balanceOf(address) for address in addresses]
 
             assert balances_after[0] - balances_before[0] == 0
             assert balances_after[1] - balances_before[1] == -0x5
             assert balances_after[2] - balances_before[2] == 0
             assert balances_after[3] - balances_before[3] == 0x5
-
-        zk_evm.state = state
+        kakarot.state = state
