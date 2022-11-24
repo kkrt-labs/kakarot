c23a8c8c541d654bbb9d0a792c5aee09cf6d2b4a
Clément Walter <clement0walter@gmail.com>
2022-11-24 09:11:46 +0100

    Use pytest in all unit tests

--- tests/units/instructions/test_block_information.py
+++ tests/units/instructions/test_block_information.py
@@ -1,43 +1,24 @@
-from asyncio import run
-from unittest import IsolatedAsyncioTestCase
-
-from cairo_coverage import cairo_coverage
-from starkware.starknet.business_logic.state.state_api_objects import BlockInfo
+import pytest
+import pytest_asyncio
 from starkware.starknet.testing.starknet import Starknet
 
 
-class TestBlockInformation(IsolatedAsyncioTestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        async def _setUpClass(cls) -> None:
-            cls.starknet = await Starknet.empty()
-            cls.starknet.state.state.update_block_info(
-                BlockInfo.create_for_testing(block_number=1, block_timestamp=1)
-            )
-            cls.test_block_informations = await cls.starknet.deploy(
-                source="./tests/cairo_files/instructions/test_block_information.cairo",
-                cairo_path=["src"],
-                disable_hint_validation=False,
-            )
+@pytest_asyncio.fixture(scope="module")
+async def block_information(starknet: Starknet):
+    return await starknet.deploy(
+        source="./tests/cairo_files/instructions/test_block_information.cairo",
+        cairo_path=["src"],
+        disable_hint_validation=False,
+    )
 
-        run(_setUpClass(cls))
 
-    async def coverageSetupClass(cls):
-        cls.test_block_informations = await cls.starknet.deploy(
-            source="./tests/cairo_files/instructions/test_block_information.cairo",
-            cairo_path=["src"],
-            disable_hint_validation=False,
-        )
-
-    @classmethod
-    def tearDownClass(cls):
-        cairo_coverage.report_runs(excluded_file={"site-packages"})
-
-    async def test_everything_block(self):
-        await self.test_block_informations.test__chainId__should_push_chain_id_to_stack().call()
-        await self.test_block_informations.test__coinbase_should_push_coinbase_address_to_stack().call()
-        await self.test_block_informations.test__timestamp_should_push_block_timestamp_to_stack().call()
-        await self.test_block_informations.test__number_should_push_block_number_to_stack().call()
-        await self.test_block_informations.test__gaslimit_should_push_gaslimit_to_stack().call()
-        await self.test_block_informations.test__difficulty_should_push_difficulty_to_stack().call()
-        await self.test_block_informations.test__basefee_should_push_basefee_to_stack().call()
+@pytest.mark.asyncio
+class TestBlockInformation:
+    async def test_everything_block(self, block_information):
+        await block_information.test__chainId__should_push_chain_id_to_stack().call()
+        await block_information.test__coinbase_should_push_coinbase_address_to_stack().call()
+        await block_information.test__timestamp_should_push_block_timestamp_to_stack().call()
+        await block_information.test__number_should_push_block_number_to_stack().call()
+        await block_information.test__gaslimit_should_push_gaslimit_to_stack().call()
+        await block_information.test__difficulty_should_push_difficulty_to_stack().call()
+        await block_information.test__basefee_should_push_basefee_to_stack().call()
--- tests/units/instructions/test_memory_operations.py
+++ tests/units/instructions/test_memory_operations.py
@@ -1,48 +1,28 @@
-from asyncio import run
-from unittest import IsolatedAsyncioTestCase
-
-from cairo_coverage import cairo_coverage
-from starkware.starknet.business_logic.state.state_api_objects import BlockInfo
+import pytest
+import pytest_asyncio
 from starkware.starknet.testing.starknet import Starknet
 
 
-class TestMemoryOperations(IsolatedAsyncioTestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        async def _setUpClass(cls) -> None:
-            cls.starknet = await Starknet.empty()
-            cls.starknet.state.state.update_block_info(
-                BlockInfo.create_for_testing(block_number=1, block_timestamp=1)
-            )
-            cls.test_memory_operations = await cls.starknet.deploy(
-                source="./tests/cairo_files/instructions/test_memory_operations.cairo",
-                cairo_path=["src"],
-                disable_hint_validation=False,
-            )
+@pytest_asyncio.fixture(scope="module")
+async def memory_operations(starknet: Starknet):
+    return await starknet.deploy(
+        source="./tests/cairo_files/instructions/test_memory_operations.cairo",
+        cairo_path=["src"],
+        disable_hint_validation=False,
+    )
 
-        run(_setUpClass(cls))
 
-    async def coverageSetupClass(cls):
-        cls.test_memory_operations = await cls.starknet.deploy(
-            source="./tests/cairo_files/instructions/test_memory_operations.cairo",
-            cairo_path=["src"],
-            disable_hint_validation=False,
-        )
-
-    @classmethod
-    def tearDownClass(cls):
-        cairo_coverage.report_runs(excluded_file={"site-packages"})
-
-    async def test_everything_memory(self):
-        # aliexpress fuzzing
+@pytest.mark.asyncio
+class TestMemoryOperations:
+    async def test_everything_memory(self, memory_operations):
         [
-            await self.test_memory_operations.test__exec_pc__should_update_after_incrementing(
+            await memory_operations.test__exec_pc__should_update_after_incrementing(
                 increment=x
             ).call()
             for x in range(1, 15)
         ]
-        await self.test_memory_operations.test__exec_pop_should_pop_an_item_from_execution_context().call()
-        await self.test_memory_operations.test__exec_mload_should_load_a_value_from_memory().call()
-        await self.test_memory_operations.test__exec_mload_should_load_a_value_from_memory_with_memory_expansion().call()
-        await self.test_memory_operations.test__exec_mload_should_load_a_value_from_memory_with_offset_larger_than_msize().call()
-        await self.test_memory_operations.test__exec_gas_should_return_remaining_gas().call()
+        await memory_operations.test__exec_pop_should_pop_an_item_from_execution_context().call()
+        await memory_operations.test__exec_mload_should_load_a_value_from_memory().call()
+        await memory_operations.test__exec_mload_should_load_a_value_from_memory_with_memory_expansion().call()
+        await memory_operations.test__exec_mload_should_load_a_value_from_memory_with_offset_larger_than_msize().call()
+        await memory_operations.test__exec_gas_should_return_remaining_gas().call()
