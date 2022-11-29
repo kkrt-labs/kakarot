eb8b9ba7cbde0de1c54e6fa652f9eea1f5b203d5
Clément Walter <clement0walter@gmail.com>
2022-11-29 12:46:15 +0100

    Use different bytecode_len in test

--- tests/units/test_contract_account.py
+++ tests/units/test_contract_account.py
@@ -22,11 +22,13 @@ async def contract_account(starknet: Starknet):
 
 @pytest.mark.asyncio
 class TestContractAccount:
-    async def test_should_store_code(self, contract_account: StarknetContract):
-        bytecode_len = 10
+    @pytest.mark.parametrize("bytecode_len", [0, 15, 16, 17, 30, 31, 32, 33])
+    async def test_should_store_code(
+        self, contract_account: StarknetContract, bytecode_len
+    ):
         bytecode = [random.randint(0, 255) for _ in range(bytecode_len)]
 
         with traceit.context("contract_account"):
             await contract_account.write_bytecode(bytecode).execute(caller_address=1)
-        stored_bytecode = await contract_account.bytecode().call()
-        assert stored_bytecode.result.bytecode == bytecode
+        stored_bytecode = (await contract_account.bytecode().call()).result.bytecode
+        assert stored_bytecode == bytecode
