16ae78e16c9faac3cd79a0efd8dfbdf4928e2fc9
jhnn <johann.bestowrous@gmail.com>
2022-11-30 06:17:13 -0800

    add coverage for swap8

--- tests/units/instructions/test_exchange_operations.py
+++ tests/units/instructions/test_exchange_operations.py
@@ -6,7 +6,7 @@ from starkware.starknet.testing.starknet import Starknet
 
 
 @pytest_asyncio.fixture(scope="module")
-async def execution_context(starknet: Starknet):
+async def exchange_operations(starknet: Starknet):
     return await starknet.deploy(
         source="./tests/cairo_files/instructions/test_exchange_operations.cairo",
         cairo_path=["src"],
@@ -16,17 +16,24 @@ async def execution_context(starknet: Starknet):
 
 @pytest.mark.asyncio
 class TestExchangeOperationst:
-    async def test_everything_context(self, execution_context):
-        await execution_context.test__util_prepare_stack__should_create_stack_with_top_and_preswapped_elements().call()
-        await execution_context.test__exec_swap1__should_swap_1st_and_2nd().call()
-        await execution_context.test__exec_swap2__should_swap_1st_and_3rd().call()
+    async def test_everything_context(self, exchange_operations):
+        await exchange_operations.test__util_prepare_stack__should_create_stack_with_top_and_preswapped_elements().call()
+        await exchange_operations.test__exec_swap1__should_swap_1st_and_2nd().call()
+        await exchange_operations.test__exec_swap2__should_swap_1st_and_3rd().call()
+        await exchange_operations.test__exec_swap8__should_swap_1st_and_9th().call()
 
         with pytest.raises(Exception) as e:
-            await execution_context.test__exec_swap1__should_fail__when_index_1_is_underflow().call()
+            await exchange_operations.test__exec_swap1__should_fail__when_index_1_is_underflow().call()
         message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
         assert message == "Kakarot: StackUnderflow"
 
         with pytest.raises(Exception) as e:
-            await execution_context.test__exec_swap2__should_fail__when_index_2_is_underflow().call()
+            await exchange_operations.test__exec_swap2__should_fail__when_index_2_is_underflow().call()
         message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
         assert message == "Kakarot: StackUnderflow"
+
+        with pytest.raises(Exception) as e:
+            await exchange_operations.test__exec_swap8__should_fail__when_index_8_is_underflow().call()
+        message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+        assert message == "Kakarot: StackUnderflow"
+
