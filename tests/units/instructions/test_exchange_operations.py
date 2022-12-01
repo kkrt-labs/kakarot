ab6cdb54f55a1e33a7cfef69b2a00e707644f080
jhnn <johann.bestowrous@gmail.com>
2022-11-30 18:51:05 -0800

    test all the way through `exec_swap16`

--- tests/units/instructions/test_exchange_operations.py
+++ tests/units/instructions/test_exchange_operations.py
@@ -22,8 +22,13 @@ class TestExchangeOperations:
         await exchange_operations.test__exec_swap2__should_swap_1st_and_3rd().call()
         await exchange_operations.test__exec_swap8__should_swap_1st_and_9th().call()
         await exchange_operations.test__exec_swap9__should_swap_1st_and_10th().call()
-        
-       # await exchange_operations.test__exec_swap16__should_swap_1st_and_17th().call()
+        await exchange_operations.test__exec_swap10__should_swap_1st_and_11th().call()
+        await exchange_operations.test__exec_swap11__should_swap_1st_and_12th().call()
+        await exchange_operations.test__exec_swap12__should_swap_1st_and_13th().call()        
+        await exchange_operations.test__exec_swap13__should_swap_1st_and_14th().call() 
+        await exchange_operations.test__exec_swap14__should_swap_1st_and_15th().call()
+        await exchange_operations.test__exec_swap15__should_swap_1st_and_16th().call()                
+        await exchange_operations.test__exec_swap16__should_swap_1st_and_17th().call()
 
         
         with pytest.raises(Exception) as e:
@@ -44,10 +49,42 @@ class TestExchangeOperations:
         with pytest.raises(Exception) as e:
             await exchange_operations.test__exec_swap9__should_fail__when_index_9_is_underflow().call()
         message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+        assert message == "Kakarot: StackUnderflow"
+
+        with pytest.raises(Exception) as e:
+            await exchange_operations.test__exec_swap10__should_fail__when_index_10_is_underflow().call()
+        message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+        assert message == "Kakarot: StackUnderflow"
+
+        with pytest.raises(Exception) as e:
+            await exchange_operations.test__exec_swap11__should_fail__when_index_11_is_underflow().call()
+        message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+        assert message == "Kakarot: StackUnderflow"
+
+        with pytest.raises(Exception) as e:
+            await exchange_operations.test__exec_swap12__should_fail__when_index_12_is_underflow().call()
+        message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
         assert message == "Kakarot: StackUnderflow"        
+
+        with pytest.raises(Exception) as e:
+            await exchange_operations.test__exec_swap13__should_fail__when_index_13_is_underflow().call()
+        message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+        assert message == "Kakarot: StackUnderflow"        
+
+        with pytest.raises(Exception) as e:
+            await exchange_operations.test__exec_swap14__should_fail__when_index_14_is_underflow().call()
+        message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+        assert message == "Kakarot: StackUnderflow"        
+
+        with pytest.raises(Exception) as e:
+            await exchange_operations.test__exec_swap15__should_fail__when_index_15_is_underflow().call()
+        message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+        assert message == "Kakarot: StackUnderflow"        
+               
 
         with pytest.raises(Exception) as e:
             await exchange_operations.test__exec_swap16__should_fail__when_index_16_is_underflow().call()
         message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
         assert message == "Kakarot: StackUnderflow"
+
         
