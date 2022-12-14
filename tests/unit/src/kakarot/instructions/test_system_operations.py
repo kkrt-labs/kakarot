8dc6b1ddd15a31d817bc304398c9276438dff3e2
TurcFort07 <boramindas@gmail.com>
2022-12-14 19:56:38 +0100

    uncomment

--- tests/unit/src/kakarot/instructions/test_system_operations.py
+++ tests/unit/src/kakarot/instructions/test_system_operations.py
@@ -35,45 +35,37 @@ class TestSystemOperations:
     async def test_return(self, system_operations):
         await system_operations.test__exec_return_should_return_context_with_updated_return_data(1000).call()
 
-    # async def test_call(
-    #     self, system_operations, contract_account_class, account_registry
-    # ):
-    #     await system_operations.test__exec_call__should_return_a_new_context_based_on_calling_ctx_stack(
-    #         contract_account_class.class_hash, account_registry.contract_address
-    #     ).call()
-
-    #     await system_operations.test__exec_callcode__should_return_a_new_context_based_on_calling_ctx_stack(
-    #         contract_account_class.class_hash, account_registry.contract_address
-    #     ).call()
-
-    #     await system_operations.test__exec_staticcall__should_return_a_new_context_based_on_calling_ctx_stack(
-    #         contract_account_class.class_hash, account_registry.contract_address
-    #     ).call()
-
-    #     await system_operations.test__exec_delegatecall__should_return_a_new_context_based_on_calling_ctx_stack(
-    #         contract_account_class.class_hash, account_registry.contract_address
-    #     ).call()
-
-    # async def test_create(
-    #     self, system_operations, contract_account_class, account_registry
-    # ):
-    #     await system_operations.test__exec_create__should_return_a_new_context_with_bytecode_from_memory_at_empty_address(
-    #         contract_account_class.class_hash,
-    #         account_registry.contract_address,
-    #     ).call()
-
-    # async def test_create2(
-    #     self, system_operations, contract_account_class, account_registry
-    # ):
-    #     await system_operations.test__exec_create2__should_return_a_new_context_with_bytecode_from_memory_at_empty_address(
-    #         contract_account_class.class_hash,
-    #         account_registry.contract_address,
-    #     ).call()
-    async def test_selfdestruct(
-        self, system_operations, contract_account_class, account_registry, eth
+    async def test_call(
+        self, system_operations, contract_account_class, account_registry
     ):
-        await system_operations.test__exec_selfdestruct__should_delete_account_bytecode(
-            eth.contract_address,
+        await system_operations.test__exec_call__should_return_a_new_context_based_on_calling_ctx_stack(
+            contract_account_class.class_hash, account_registry.contract_address
+        ).call()
+
+        await system_operations.test__exec_callcode__should_return_a_new_context_based_on_calling_ctx_stack(
+            contract_account_class.class_hash, account_registry.contract_address
+        ).call()
+
+        await system_operations.test__exec_staticcall__should_return_a_new_context_based_on_calling_ctx_stack(
+            contract_account_class.class_hash, account_registry.contract_address
+        ).call()
+
+        await system_operations.test__exec_delegatecall__should_return_a_new_context_based_on_calling_ctx_stack(
+            contract_account_class.class_hash, account_registry.contract_address
+        ).call()
+
+    async def test_create(
+        self, system_operations, contract_account_class, account_registry
+    ):
+        await system_operations.test__exec_create__should_return_a_new_context_with_bytecode_from_memory_at_empty_address(
+            contract_account_class.class_hash,
+            account_registry.contract_address,
+        ).call()
+
+    async def test_create2(
+        self, system_operations, contract_account_class, account_registry
+    ):
+        await system_operations.test__exec_create2__should_return_a_new_context_with_bytecode_from_memory_at_empty_address(
             contract_account_class.class_hash,
             account_registry.contract_address,
         ).call()
