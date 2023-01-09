afaaf5e7939d934aa2d3ed6af9bbdb4c55a0c5d6
Clément Walter <clement0walter@gmail.com>
2023-01-09 20:18:14 +0100

    Add Univ2 ERC20 test

--- tests/integration/helpers/helpers.py
+++ tests/integration/helpers/helpers.py
@@ -1,5 +1,16 @@
+import random
 from textwrap import wrap
-from typing import List
+from typing import Tuple
+
+from eth_abi import encode_abi
+from eth_keys import keys
+from eth_utils import decode_hex, keccak, to_checksum_address
+
+from tests.integration.helpers.constants import CHAIN_ID
+
+PERMIT_TYPEHASH = keccak(
+    text="Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)"
+)
 
 
 def hex_string_to_bytes_array(h: str):
@@ -43,3 +54,96 @@ def int_to_uint256(value):
     low = value & ((1 << 128) - 1)
     high = value >> 128
     return low, high
+
+
+def expand_to_18_decimals(n: int) -> int:
+    return n * 10**18
+
+
+def get_domain_separator(name: str, token_address: str) -> bytes:
+    return keccak(
+        encode_abi(
+            ["bytes32", "bytes32", "bytes32", "uint256", "address"],
+            [
+                keccak(
+                    text="EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak(text=name),
+                keccak(text="1"),
+                CHAIN_ID,
+                token_address,
+            ],
+        )
+    )
+
+
+def get_create2_address(
+    factory_address: str, token_a: str, token_b: str, bytecode: bytes
+) -> str:
+    token_0, token_1 = sorted([token_a, token_b])
+    create2_inputs = [
+        b"\xff",
+        decode_hex(factory_address),
+        keccak(
+            encode_abi(
+                ["address", "address"],
+                [token_0, token_1],
+            )
+        ),
+        keccak(bytecode),
+    ]
+    sanitized_inputs = b"".join(create2_inputs[2:])
+    return to_checksum_address(keccak(sanitized_inputs)[-40:])
+
+
+def get_approval_digest(
+    token_name: str, token_address: str, approve: dict, nonce: int, deadline: int
+) -> bytes:
+    domain_separator = get_domain_separator(token_name, token_address)
+    return keccak(
+        b"\x19"
+        + b"\x01"
+        + domain_separator
+        + keccak(
+            encode_abi(
+                [
+                    "bytes32",
+                    "address",
+                    "address",
+                    "uint256",
+                    "uint256",
+                    "uint256",
+                ],
+                [
+                    PERMIT_TYPEHASH,
+                    approve["owner"],
+                    approve["spender"],
+                    approve["value"],
+                    nonce,
+                    deadline,
+                ],
+            )
+        ),
+    )
+
+
+def encode_price(reserve_0: int, reserve_1: int) -> list:
+    return [
+        reserve_1 * 2**112 // reserve_0,
+        reserve_0 * 2**112 // reserve_1,
+    ]
+
+
+def generate_random_private_key():
+    return keys.PrivateKey(int.to_bytes(random.getrandbits(256), 32, "big"))
+
+
+def ec_sign(
+    digest: bytes, owner_private_key: keys.PrivateKey
+) -> Tuple[int, bytes, bytes]:
+    signature = owner_private_key.sign_msg_hash(digest)
+    return (
+        signature.v + 27,
+        bytes.fromhex(f"{signature.r:x}"),
+        bytes.fromhex(f"{signature.s:x}"),
+    )
