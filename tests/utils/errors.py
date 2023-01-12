3a474d9fd7eeefc466ca9cab553ce4cb328c5596
Clément Walter <clement0walter@gmail.com>
2023-01-12 09:28:47 +0100

    Fix kakarot_error not parsing only bytes

--- tests/utils/errors.py
+++ tests/utils/errors.py
@@ -10,6 +10,12 @@ def kakarot_error(message):
         with pytest.raises(Exception) as e:
             yield e
         error = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
-        assert message == bytes.fromhex(f"{int(error):x}").decode()
+        if re.match("Kakarot: Reverted with reason: ", error):
+            error = re.search(r"Kakarot: Reverted with reason: (.*)", error)[1]  # type: ignore
+            error_hex = f"{int(error):x}"
+            error_hex = "0" + error_hex if len(error_hex) % 2 == 1 else error_hex
+            assert message == bytes.fromhex(error_hex).decode()
+        else:
+            assert message == error
     finally:
         pass
