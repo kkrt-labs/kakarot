43a3adc650139d52d383428f98b172281b710fe6
Flydexo <flydexo@gmail.com>
2023-01-12 23:02:37 +0100

    fix: typo and native functions

--- tests/integration/eoa/test_ethaa.py
+++ tests/integration/eoa/test_ethaa.py
@@ -17,40 +17,38 @@ from tests.utils.signer import MockEthSigner
 @pytest.mark.asyncio
 class TestExternallyOwnedAccount:
     class TestComputeStarknetAddress:
-        @pytest.mark.parametrize("address_idx", [i for i in range(0, 4)])
+        @pytest.mark.parametrize("address_idx", range(4))
         async def test_should_return_same_address_as_contract(
             self, deployer, addresses, address_idx
         ):
             address = addresses[address_idx]
             call_info = await deployer.compute_starknet_address(
-                evm_address=web3.Web3.toInt(hexstr=address.address)
+                evm_address=int(address.address, 16)
             ).call()
             assert (
                 call_info.result.contract_address
                 == address.starknet_contract.contract_address
             )
 
-        @pytest.mark.parametrize("address_idx", [i for i in range(0, 4)])
+        @pytest.mark.parametrize("address_idx", range(4))
         async def test_should_return_the_eth_address_used_at_deploy(
             self, deployer, addresses, address_idx
         ):
             address = addresses[address_idx]
             call_info = await address.starknet_contract.get_eth_address().call()
-            assert call_info.result.eth_address == web3.Web3.toInt(
-                hexstr=address.address
-            )
+            assert call_info.result.eth_address == int(address.address, 16)
 
     class TestEthSignature:
-        @pytest.mark.parametrize("address_idx", [i for i in range(0, 4)])
+        @pytest.mark.parametrize("address_idx", range(4))
         async def test_should_validate_signature(
             self, deployer, default_tx, addresses, address_idx
         ):
             address = addresses[address_idx]
-            tempAccount = web3.Account.from_key(address.private_key)
-            raw_tx = tempAccount.sign_transaction(default_tx)
+            tmp_account = web3.Account.from_key(address.private_key)
+            raw_tx = tmp_account.sign_transaction(default_tx)
             tx_hash = serializable_unsigned_transaction_from_dict(default_tx).hash()
             call_info = await address.starknet_contract.is_valid_signature(
-                [*int_to_uint256(web3.Web3.toInt(tx_hash))],
+                [*int_to_uint256(web.Web3.toInt(tx_hash, 16))],
                 [
                     raw_tx.v,
                     *int_to_uint256(raw_tx.r),
@@ -59,13 +57,13 @@ class TestExternallyOwnedAccount:
             ).call()
             assert call_info.result.is_valid == True
 
-        @pytest.mark.parametrize("address_idx", [i for i in range(0, 4)])
+        @pytest.mark.parametrize("address_idx", range(4))
         async def test_should_fail_when_verifying_fake_signature(
             self, deployer, default_tx, addresses, address_idx
         ):
             address = addresses[address_idx]
-            tempAccount = web3.Account.from_key(address.private_key)
-            raw_tx = tempAccount.sign_transaction(default_tx)
+            tmp_account = web3.Account.from_key(address.private_key)
+            raw_tx = tmp_account.sign_transaction(default_tx)
             tx_hash = serializable_unsigned_transaction_from_dict(default_tx).hash()
             with pytest.raises(StarkException):
                 await address.starknet_contract.is_valid_signature(
@@ -78,14 +76,14 @@ class TestExternallyOwnedAccount:
                 ).call()
 
     class TestExecute:
-        @pytest.mark.parametrize("address_idx", [i for i in range(0, 4)])
+        @pytest.mark.parametrize("address_idx", range(4))
         async def test_should_execute_tx(
             self, deployer, kakarot, default_tx, addresses, address_idx
         ):
             address = addresses[address_idx]
             eth_account = MockEthSigner(private_key=address.private_key)
-            tempAccount = web3.Account().from_key(address.private_key)
-            raw_tx = tempAccount.sign_transaction(default_tx)
+            tmp_account = web3.Account().from_key(address.private_key)
+            raw_tx = tmp_account.sign_transaction(default_tx)
 
             await eth_account.send_transaction(
                 address.starknet_contract,
@@ -94,7 +92,7 @@ class TestExternallyOwnedAccount:
                 raw_tx.rawTransaction,
             )
 
-        @pytest.mark.parametrize("address_idx", [i for i in range(0, 4)])
+        @pytest.mark.parametrize("address_idx", range(4))
         async def test_should_fail_on_incorrect_signature(
             self, deployer, kakarot, default_tx, addresses, address_idx
         ):
