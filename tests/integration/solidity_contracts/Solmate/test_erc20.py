a039c49026dca6868a6af4cb14ced37566ee87f8
johann bestowrous <johann.bestowrous@gmail.com>
2023-01-12 01:12:44 -0800

    Complete solmate Erc20 integration testing (#352) (#430)
    ## Pull request type
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [X] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## What is the current behavior?
    
    Incomplete integration test coverage for the solmates erc20 contract.
    
    Resolves #352 
    
    ## What is the new behavior?
    Richer integration testing ported from solmates
    [here](https://github.com/transmissions11/solmate/blob/main/src/test/ERC20.t.sol)
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    -
    -
    -
    
    ## Other information
    
    <!-- Give an estimate of the time you spent on this PR in terms of work
    days. Did you spend 0.5 days on this PR or rather 2 days? -->
    Time spent on this PR:  3/4s of a day
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->
    
    Co-authored-by: Clément Walter <clement0walter@gmail.com>
    Co-authored-by: kakarot CI <sayajin-labs@users.noreply.github.com>

--- tests/integration/solidity_contracts/Solmate/test_erc20.py
+++ tests/integration/solidity_contracts/Solmate/test_erc20.py
@@ -1,6 +1,23 @@
-import pytest
+import re
 
-from tests.utils.reporting import traceit
+import pytest
+from eth_utils import keccak
+
+from tests.integration.helpers.constants import MAX_INT
+from tests.integration.helpers.helpers import (
+    PERMIT_TYPEHASH,
+    ec_sign,
+    get_approval_digest,
+    get_domain_separator,
+)
+
+TEST_SUPPLY = 10**18
+TEST_AMOUNT = int(0.9 * 10**18)
+
+
+@pytest.fixture(scope="module")
+async def other(others):
+    return others[0]
 
 
 @pytest.mark.asyncio
@@ -16,37 +33,314 @@ class TestERC20:
             decimals = await erc_20.decimals()
             assert decimals == 18
 
-    async def test_should_mint_approve_and_transfer(
-        self, erc_20, owner, others, request
-    ):
-        with traceit.context(request.node.own_markers[0].name):
-
-            # Mint few token to others[0]
+    class TestMint:
+        async def test_should_mint(self, erc_20, owner, other):
             await erc_20.mint(
-                others[0].address, 356, caller_address=owner.starknet_address
+                other.address,
+                TEST_SUPPLY,
+                caller_address=owner.starknet_address,
             )
-            assert await erc_20.totalSupply() == 356
-            assert await erc_20.balanceOf(others[0].address) == 356
+            assert await erc_20.totalSupply() == TEST_SUPPLY
+            assert await erc_20.balanceOf(other.address) == TEST_SUPPLY
 
-            # others[0] approves others[1] to spend some
+    class TestBurn:
+        async def test_should_burn(self, erc_20, owner, other):
+            burn_amount = TEST_AMOUNT
+            await erc_20.mint(
+                other.address,
+                TEST_SUPPLY,
+                caller_address=owner.starknet_address,
+            )
+            await erc_20.burn(
+                other.address, burn_amount, caller_address=owner.starknet_address
+            )
+            assert await erc_20.totalSupply() == TEST_SUPPLY - burn_amount
+            assert await erc_20.balanceOf(other.address) == TEST_SUPPLY - burn_amount
+
+    class TestApprove:
+        async def test_should_approve(self, erc_20, owner, other):
+            assert await erc_20.approve(
+                other.address,
+                TEST_SUPPLY,
+                caller_address=owner.starknet_address,
+            )
+            assert await erc_20.allowance(owner.address, other.address) == TEST_SUPPLY
+
+    class TestTransfer:
+        async def test_should_transfer(self, erc_20, owner, other):
+            await erc_20.mint(
+                owner.address, TEST_SUPPLY, caller_address=owner.starknet_address
+            )
+            assert await erc_20.transfer(
+                other.address,
+                TEST_SUPPLY,
+                caller_address=owner.starknet_address,
+            )
+            assert await erc_20.totalSupply() == TEST_SUPPLY
+            assert await erc_20.balanceOf(owner.address) == 0
+            assert await erc_20.balanceOf(other.address) == TEST_SUPPLY
+
+        async def test_should_transfer_from(self, erc_20, owner, others):
+            from_wallet = others[0]
+            to_wallet = others[1]
+
+            await erc_20.mint(
+                from_wallet.address,
+                TEST_SUPPLY,
+                caller_address=owner.starknet_address,
+            )
+
             await erc_20.approve(
-                others[1].address, 10, caller_address=others[0].starknet_address
+                owner.address,
+                TEST_SUPPLY,
+                caller_address=from_wallet.starknet_address,
             )
-            assert await erc_20.allowance(others[0].address, others[1].address) == 10
+            assert await erc_20.transferFrom(
+                from_wallet.address,
+                to_wallet.address,
+                TEST_SUPPLY,
+                caller_address=owner.starknet_address,
+            )
+            assert await erc_20.totalSupply() == TEST_SUPPLY
 
-            # others[1] sends others[0] token to themselves
-            await erc_20.transferFrom(
-                others[0].address,
-                others[1].address,
-                10,
-                caller_address=others[1].starknet_address,
-            )
-            assert await erc_20.balanceOf(others[0].address) == 356 - 10
-            assert await erc_20.balanceOf(others[1].address) == 10
+            assert await erc_20.allowance(from_wallet.address, owner.address) == 0
 
-            # others[1] sends token to others[2]
-            await erc_20.transfer(
-                others[2].address, 1, caller_address=others[1].starknet_address
+            assert await erc_20.balanceOf(from_wallet.address) == 0
+            assert await erc_20.balanceOf(to_wallet.address) == TEST_SUPPLY
+
+        async def test_should_transfer_from_with_infinite_approve(
+            self, erc_20, owner, others
+        ):
+            from_wallet = others[0]
+            to_wallet = others[1]
+
+            await erc_20.mint(
+                from_wallet.address,
+                TEST_SUPPLY,
+                caller_address=owner.starknet_address,
             )
-            assert await erc_20.balanceOf(others[1].address) == 10 - 1
-            assert await erc_20.balanceOf(others[2].address) == 1
+
+            await erc_20.approve(
+                owner.address, MAX_INT, caller_address=from_wallet.starknet_address
+            )
+            assert await erc_20.transferFrom(
+                from_wallet.address,
+                to_wallet.address,
+                TEST_SUPPLY,
+                caller_address=owner.starknet_address,
+            )
+            assert await erc_20.totalSupply() == TEST_SUPPLY
+
+            assert await erc_20.allowance(from_wallet.address, owner.address) == MAX_INT
+
+            assert await erc_20.balanceOf(from_wallet.address) == 0
+            assert await erc_20.balanceOf(others[1].address) == TEST_SUPPLY
+
+        async def test_transfer_should_fail_when_insufficient_balance(
+            self, erc_20, owner, other
+        ):
+            await erc_20.mint(
+                owner.address, TEST_AMOUNT, caller_address=owner.starknet_address
+            )
+            with pytest.raises(Exception) as e:
+                await erc_20.transfer(
+                    other.address,
+                    TEST_SUPPLY,
+                    caller_address=owner.starknet_address,
+                )
+
+            message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+            # TODO: update with https://github.com/sayajin-labs/kakarot/issues/416
+            assert message == "Kakarot: Reverted with reason: 12884901888"
+
+        async def test_transfer_from_should_fail_when_insufficient_allowance(
+            self, erc_20, owner, other, others
+        ):
+            await erc_20.mint(
+                other.address, TEST_SUPPLY, caller_address=owner.starknet_address
+            )
+            await erc_20.approve(
+                owner.address,
+                TEST_AMOUNT,
+                caller_address=other.starknet_address,
+            )
+            with pytest.raises(Exception) as e:
+                await erc_20.transferFrom(
+                    other.address,
+                    others[1].address,
+                    TEST_SUPPLY,
+                    caller_address=owner.starknet_address,
+                )
+
+            message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+            # TODO: update with https://github.com/sayajin-labs/kakarot/issues/416
+            assert (
+                message
+                == "Kakarot: Reverted with reason: 109161241298996469498303502024926298112"
+            )
+
+        async def test_transfer_from_should_fail_when_insufficient_balance(
+            self, erc_20, owner, other, others
+        ):
+            await erc_20.mint(
+                other.address,
+                TEST_AMOUNT,
+                caller_address=owner.starknet_address,
+            )
+
+            await erc_20.approve(
+                owner.address,
+                TEST_SUPPLY,
+                caller_address=other.starknet_address,
+            )
+            with pytest.raises(Exception) as e:
+                await erc_20.transferFrom(
+                    other.address,
+                    others[1].address,
+                    TEST_SUPPLY,
+                    caller_address=owner.starknet_address,
+                )
+
+            message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+            # TODO: update with https://github.com/sayajin-labs/kakarot/issues/416
+            assert message == "Kakarot: Reverted with reason: 12884901888"
+
+    class TestPermit:
+        async def test_should_permit(self, blockhashes, erc_20, owner, other):
+            nonce = await erc_20.nonces(owner.address)
+            deadline = blockhashes["current_block"]["timestamp"]
+            digest = get_approval_digest(
+                "Kakarot Token",
+                erc_20.evm_contract_address,
+                {
+                    "owner": owner.address,
+                    "spender": other.address,
+                    "value": TEST_SUPPLY,
+                },
+                nonce,
+                deadline,
+            )
+            v, r, s = ec_sign(digest, owner.private_key)
+            await erc_20.permit(
+                owner.address,
+                other.address,
+                TEST_SUPPLY,
+                deadline,
+                v,
+                r,
+                s,
+                caller_address=owner.starknet_address,
+            )
+
+            assert await erc_20.allowance(owner.address, other.address) == TEST_SUPPLY
+            assert await erc_20.nonces(owner.address) == 1
+
+        async def test_permit_should_fail_with_bad_nonce(
+            self, blockhashes, erc_20, owner, other
+        ):
+            bad_nonce = 1
+            deadline = blockhashes["current_block"]["timestamp"]
+            digest = get_approval_digest(
+                "Kakarot Token",
+                erc_20.evm_contract_address,
+                {
+                    "owner": owner.address,
+                    "spender": other.address,
+                    "value": MAX_INT,
+                },
+                bad_nonce,
+                deadline,
+            )
+            v, r, s = ec_sign(digest, owner.private_key)
+            with pytest.raises(Exception) as e:
+                await erc_20.permit(
+                    owner.address,
+                    other.address,
+                    TEST_SUPPLY,
+                    deadline,
+                    v,
+                    r,
+                    s,
+                    caller_address=owner.starknet_address,
+                )
+
+            message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+            # TODO: update with https://github.com/sayajin-labs/kakarot/issues/416
+            assert message == "Kakarot: Reverted with reason: 0"
+
+        async def test_permit_should_fail_with_bad_deadline(
+            self, erc_20, blockhashes, owner, other
+        ):
+            nonce = await erc_20.nonces(owner.address)
+            deadline = blockhashes["current_block"]["timestamp"]
+            digest = get_approval_digest(
+                "Kakarot Token",
+                erc_20.evm_contract_address,
+                {
+                    "owner": owner.address,
+                    "spender": other.address,
+                    "value": MAX_INT,
+                },
+                nonce,
+                deadline,
+            )
+            v, r, s = ec_sign(digest, owner.private_key)
+            with pytest.raises(Exception) as e:
+                await erc_20.permit(
+                    owner.address,
+                    other.address,
+                    TEST_SUPPLY,
+                    deadline - 1,
+                    v,
+                    r,
+                    s,
+                    caller_address=owner.starknet_address,
+                )
+
+            message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+            # TODO: update with https://github.com/sayajin-labs/kakarot/issues/416
+            assert message == "Kakarot: Reverted with reason: 147028384"
+
+        async def test_permit_should_fail_on_replay(
+            self, blockhashes, erc_20, owner, other
+        ):
+            nonce = await erc_20.nonces(owner.address)
+            deadline = blockhashes["current_block"]["timestamp"]
+            digest = get_approval_digest(
+                "Kakarot Token",
+                erc_20.evm_contract_address,
+                {
+                    "owner": owner.address,
+                    "spender": other.address,
+                    "value": TEST_SUPPLY,
+                },
+                nonce,
+                deadline,
+            )
+            v, r, s = ec_sign(digest, owner.private_key)
+            await erc_20.permit(
+                owner.address,
+                other.address,
+                TEST_SUPPLY,
+                deadline,
+                v,
+                r,
+                s,
+                caller_address=owner.starknet_address,
+            )
+
+            with pytest.raises(Exception) as e:
+                await erc_20.permit(
+                    owner.address,
+                    other.address,
+                    TEST_SUPPLY,
+                    deadline,
+                    v,
+                    r,
+                    s,
+                    caller_address=owner.starknet_address,
+                )
+
+            message = re.search(r"Error message: (.*)", e.value.message)[1]  # type: ignore
+            # TODO: update with https://github.com/sayajin-labs/kakarot/issues/416
+            assert message == "Kakarot: Reverted with reason: 0"
