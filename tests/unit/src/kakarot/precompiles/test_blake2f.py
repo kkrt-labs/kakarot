3b72bf1ba4f3fcf63946fbb348b4b0a5cb3229de
kakarot CI <sayajin-labs@users.noreply.github.com>
2023-01-14 13:52:14 +0000

    CI Formatting Auto Commit

--- tests/unit/src/kakarot/precompiles/test_blake2f.py
+++ tests/unit/src/kakarot/precompiles/test_blake2f.py
@@ -21,18 +21,20 @@ class TestBlake2f:
     async def test_should_fail_when_input_len_is_not_213(self, blake2f):
         with pytest.raises(Exception) as e:
             await blake2f.test_should_fail_when_input_is_not_213().call()
-        message = re.search(r"Error message: (.*)", e.value.message)[1] 
+        message = re.search(r"Error message: (.*)", e.value.message)[1]
         assert (
             message
             == "Kakarot: blake2f failed with incorrect input_len: 212 instead of 213"
         )
+
     async def test_should_fail_when_flag_is_not_0_or_1(self, blake2f):
         with pytest.raises(Exception) as e:
             await blake2f.test_should_fail_when_flag_is_not_0_or_1().call()
-        message = re.search(r"Error message: (.*)", e.value.message)[1] 
+        message = re.search(r"Error message: (.*)", e.value.message)[1]
         assert (
             message
             == "Kakarot: blake2f failed with incorrect flag: 2 instead of 0 or 1"
         )
+
     async def test_should_return_blake2f_compression(self, blake2f):
         await blake2f.test_should_return_blake2f_compression().call()
