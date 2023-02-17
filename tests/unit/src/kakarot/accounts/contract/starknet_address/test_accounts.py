6b258c869b3fdc46d61b02e473517633a9bb3f4e
Dragan Pilipovic <draganpilipovic@Dragans-MacBook-Pro.local>
2023-02-17 02:09:13 +0100

    add test

---
+++ tests/unit/src/kakarot/accounts/contract/starknet_address/test_accounts.py
@@ -0,0 +1,41 @@
+import pytest
+import logging
+import random
+from starkware.starknet.testing.starknet import Starknet
+
+import pytest
+from starkware.starknet.testing.contract import StarknetContract
+
+from tests.integration.helpers.helpers import (
+    generate_random_private_key,
+)
+
+logger = logging.getLogger()
+
+
+
+random.seed(0)
+
+@pytest.mark.asyncio
+class TestStarknetAccount:
+    async def test_compute_starknet_address(
+        self, starknet: Starknet, kakarot: StarknetContract
+    ):
+        private_key = generate_random_private_key()
+        evm_address = private_key.public_key.to_checksum_address()
+        eoa_deploy_tx = await kakarot.deploy_externally_owned_account(
+            int(evm_address, 16)
+        ).execute()
+
+        kakarot_starknet_address = eoa_deploy_tx.call_info.internal_calls[0].contract_address
+
+        test_cairo_file = await starknet.deploy(
+            source="./tests/unit/src/kakarot/accounts/contract/starknet_address/test_accounts.cairo",
+            cairo_path=["src"],
+            disable_hint_validation=True,
+        )
+
+        computed_starknet_address = (await test_cairo_file.test__compute_starknet_address(evm_address=int(evm_address, 16)).call()).result[0]
+
+
+        assert computed_starknet_address == kakarot_starknet_address
