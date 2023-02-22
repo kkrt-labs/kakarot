9c9bcc6424cda29c0e2df8597911eb3bdb47caf4
Gregory Edison <gregory.edison1993@gmail.com>
2023-02-22 22:46:14 +0100

    test: get_multicall_from_evm_txs helper

--- tests/unit/src/kakarot/accounts/eoa/test_library.py
+++ tests/unit/src/kakarot/accounts/eoa/test_library.py
@@ -3,7 +3,7 @@ import pytest_asyncio
 from eth_account import Account
 
 from tests.utils.constants import TRANSACTIONS
-from tests.utils.helpers import generate_random_private_key
+from tests.utils.helpers import generate_random_private_key, get_multicall_from_evm_txs
 
 
 @pytest_asyncio.fixture(scope="module")
@@ -29,59 +29,30 @@ async def mock_externally_owned_account(starknet, mock_kakarot):
 @pytest.mark.asyncio
 class TestLibrary:
     async def test_execute_should_make_all_calls_and_return_concat_results(
-        self, mock_externally_owned_account
+        self, mock_externally_owned_account, eth
     ):
         private_key = generate_random_private_key()
-        calls = []
-        calldata = b""
-        expected_result = []
-        for transaction in TRANSACTIONS:
-            tx = Account.sign_transaction(transaction, private_key)["rawTransaction"]
-            calls += [
-                (
-                    0x0,  # to
-                    0x0,  # selector
-                    len(calldata),  # data_offset
-                    len(tx),  # data_len
-                )
-            ]
-            calldata += tx
-            # Execute contract bytecode
-            if transaction["to"] != "":
-                expected_result += [
-                    int(transaction["to"], 16),
-                    transaction["value"],
-                    transaction["gas"],
-                    len(
-                        bytes.fromhex(transaction.get("data", "").replace("0x", ""))
-                    ),  # calldata_len
-                    *list(
-                        bytes.fromhex(transaction.get("data", "").replace("0x", ""))
-                    ),  # calldata
-                ]
-            # Deploy Contract
-            else:
-                expected_result += [
-                    len(
-                        bytes.fromhex(transaction.get("data", "").replace("0x", ""))
-                    ),  # calldata_len
-                    *list(
-                        bytes.fromhex(transaction.get("data", "").replace("0x", ""))
-                    ),  # calldata
-                ]
+        (calls, calldata, expected_result) = get_multicall_from_evm_txs(
+            TRANSACTIONS, private_key
+        )
+        total_transferred_value = sum([x["value"] for x in TRANSACTIONS])
+
+        # Mint tokens to the EOA
+        await eth.mint(
+            mock_externally_owned_account.contract_address, (total_transferred_value, 0)
+        ).execute()
 
         assert (
             await mock_externally_owned_account.execute(calls, list(calldata)).call()
         ).result.response == expected_result
 
-    async def test_should_transfer_value_to_destination_account(
+    async def test_should_transfer_value_to_destination_address(
         self, mock_kakarot, mock_externally_owned_account, eth
     ):
         private_key = generate_random_private_key()
-        calls = []
-        calldata = b""
 
         txs = [t for t in TRANSACTIONS if t["to"] != ""]
+        (calls, calldata, _) = get_multicall_from_evm_txs(txs, private_key)
         total_transferred_value = sum([x["value"] for x in txs])
 
         evm_to_starknet_address = dict()
@@ -93,37 +64,26 @@ class TestLibrary:
         ).execute()
 
         for transaction in txs:
-            tx = Account.sign_transaction(transaction, private_key)["rawTransaction"]
-            calls += [
-                (
-                    0x0,  # to
-                    0x0,  # selector
-                    len(calldata),  # data_offset
-                    len(tx),  # data_len
-                )
-            ]
-            calldata += tx
-
             # Update expected balances
-            to_evm_address = int(transaction["to"], 16)
-            expected_balances[to_evm_address] = (
-                int(expected_balances.get(to_evm_address, 0)) + transaction["value"]
+            evm_address = int(transaction["to"], 16)
+            expected_balances[evm_address] = (
+                int(expected_balances.get(evm_address, 0)) + transaction["value"]
             )
 
             # Update address mapping
-            if to_evm_address not in evm_to_starknet_address:
-                to_starknet_address = (
-                    await mock_kakarot.compute_starknet_address(to_evm_address).call()
+            if evm_address not in evm_to_starknet_address:
+                starknet_address = (
+                    await mock_kakarot.compute_starknet_address(evm_address).call()
                 ).result.contract_address
-                evm_to_starknet_address[to_evm_address] = to_starknet_address
+                evm_to_starknet_address[evm_address] = starknet_address
 
         # execute the multicall
         await mock_externally_owned_account.execute(calls, list(calldata)).execute()
 
         # verify the value was transfered
-        for to_evm_address, amount in expected_balances.items():
+        for evm_address, amount in expected_balances.items():
             assert (
-                await eth.balanceOf(evm_to_starknet_address[to_evm_address]).call()
+                await eth.balanceOf(evm_to_starknet_address[evm_address]).call()
             ).result.balance.low == amount
         # verify EOA is empty
         assert (
