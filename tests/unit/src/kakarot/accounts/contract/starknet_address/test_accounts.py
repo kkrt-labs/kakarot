858f523d0e967e0df62375f0ebdb946bb493079e
Dragan Pilipovic <draganpilipovic@Dragans-MacBook-Pro.local>
2023-02-22 19:50:26 +0100

    refactor file

--- tests/unit/src/kakarot/accounts/contract/starknet_address/test_accounts.py
+++ tests/unit/src/kakarot/accounts/contract/starknet_address/test_accounts.py
@@ -5,8 +5,10 @@ from tests.utils.helpers import generate_random_private_key
 
 
 @pytest.mark.asyncio
-class TestStarknetAccount:
-    async def test_compute_starknet_address(self, kakarot: StarknetContract):
+class TestComputeStarknetAddress:
+    async def test_should_return_same_as_deployed_address(
+        self, kakarot: StarknetContract
+    ):
         private_key = generate_random_private_key()
         evm_address = private_key.public_key.to_checksum_address()
         eoa_deploy_tx = await kakarot.deploy_externally_owned_account(
@@ -17,9 +19,10 @@ class TestStarknetAccount:
             0
         ].contract_address
 
-        test_compute_starknet_address = await kakarot.compute_starknet_address(
-            evm_address=int(evm_address, 16)
-        ).execute()
+        computed_starknet_address = (
+            (await kakarot.compute_starknet_address(evm_address=int(evm_address, 16)))
+            .call()
+            .result[0]
+        )
 
-        computed_starknet_address = test_compute_starknet_address.result[0]
         assert computed_starknet_address == kakarot_starknet_address
