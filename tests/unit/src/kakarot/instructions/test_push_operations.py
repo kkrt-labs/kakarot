91999728588bfa9efb047b16c2972dfa4e83838b
Daniel Bejarano <58019353+dbejarano820@users.noreply.github.com>
2023-02-25 12:54:11 -0600

    Overflow tests push operations (#530)
    <!--- Please provide a general summary of your changes in the title
    above -->
    
    <!-- Give an estimate of the time you spent on this PR in terms of work
    days. Did you spend 0.5 days on this PR or rather 2 days? -->
    
    Time spent on this PR: 1 day
    
    ## Pull request type
    
    <!-- Please try to limit your pull request to one type, submit multiple
    pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [X] Other (please describe): Adding more tests
    
    ## What is the current behavior?
    
    <!-- Please describe the current behavior that you are modifying, or
    link to a relevant issue. -->
    
    Resolves #519 #520 #521 and #522 
    
    ## What is the new behavior?
    
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    Testing for overflow when carrying out different push operation to the
    stack.
    
    ## Other information
    
    <!-- Any other information that is important to this PR such as
    screenshots of how the component looks before and after the change. -->
    
    <img width="564" alt="image"
    src="https://user-images.githubusercontent.com/58019353/221092918-47a3d3fe-fc2a-476f-9296-a96a6f5ae18f.png">

--- tests/unit/src/kakarot/instructions/test_push_operations.py
+++ tests/unit/src/kakarot/instructions/test_push_operations.py
@@ -2,6 +2,9 @@ import pytest
 import pytest_asyncio
 from starkware.starknet.testing.starknet import Starknet
 
+from tests.utils.errors import kakarot_error
+from tests.utils.uint256 import int_to_uint256
+
 
 @pytest_asyncio.fixture(scope="module")
 async def push_operations(starknet: Starknet):
@@ -14,98 +17,12 @@ async def push_operations(starknet: Starknet):
 
 @pytest.mark.asyncio
 class TestPushOperations:
-    async def test__exec_push1_should_add_1_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push1_should_add_1_byte_to_stack().call()
+    @pytest.mark.parametrize("i", range(2, 33))
+    async def test__exec_push_should_raise(self, push_operations, i):
+        with kakarot_error():
+            await push_operations.test__exec_push_should_raise(i).call()
 
-    async def test__exec_push2_should_add_2_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push2_should_add_2_byte_to_stack().call()
-
-    async def test__exec_push3_should_add_3_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push3_should_add_3_byte_to_stack().call()
-
-    async def test__exec_push4_should_add_4_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push4_should_add_4_byte_to_stack().call()
-
-    async def test__exec_push5_should_add_5_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push5_should_add_5_byte_to_stack().call()
-
-    async def test__exec_push6_should_add_6_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push6_should_add_6_byte_to_stack().call()
-
-    async def test__exec_push7_should_add_7_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push7_should_add_7_byte_to_stack().call()
-
-    async def test__exec_push8_should_add_8_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push8_should_add_8_byte_to_stack().call()
-
-    async def test__exec_push9_should_add_9_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push9_should_add_9_byte_to_stack().call()
-
-    async def test__exec_push10_should_add_10_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push10_should_add_10_byte_to_stack().call()
-
-    async def test__exec_push11_should_add_11_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push11_should_add_11_byte_to_stack().call()
-
-    async def test__exec_push12_should_add_12_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push12_should_add_12_byte_to_stack().call()
-
-    async def test__exec_push13_should_add_13_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push13_should_add_13_byte_to_stack().call()
-
-    async def test__exec_push14_should_add_14_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push14_should_add_14_byte_to_stack().call()
-
-    async def test__exec_push15_should_add_15_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push15_should_add_15_byte_to_stack().call()
-
-    async def test__exec_push16_should_add_16_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push16_should_add_16_byte_to_stack().call()
-
-    async def test__exec_push17_should_add_17_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push17_should_add_17_byte_to_stack().call()
-
-    async def test__exec_push18_should_add_18_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push18_should_add_18_byte_to_stack().call()
-
-    async def test__exec_push19_should_add_19_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push19_should_add_19_byte_to_stack().call()
-
-    async def test__exec_push20_should_add_20_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push20_should_add_20_byte_to_stack().call()
-
-    async def test__exec_push21_should_add_21_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push21_should_add_21_byte_to_stack().call()
-
-    async def test__exec_push22_should_add_22_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push22_should_add_22_byte_to_stack().call()
-
-    async def test__exec_push23_should_add_23_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push23_should_add_23_byte_to_stack().call()
-
-    async def test__exec_push24_should_add_24_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push24_should_add_24_byte_to_stack().call()
-
-    async def test__exec_push25_should_add_25_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push25_should_add_25_byte_to_stack().call()
-
-    async def test__exec_push26_should_add_26_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push26_should_add_26_byte_to_stack().call()
-
-    async def test__exec_push27_should_add_27_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push27_should_add_27_byte_to_stack().call()
-
-    async def test__exec_push28_should_add_28_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push28_should_add_28_byte_to_stack().call()
-
-    async def test__exec_push29_should_add_29_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push29_should_add_29_byte_to_stack().call()
-
-    async def test__exec_push30_should_add_30_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push30_should_add_30_byte_to_stack().call()
-
-    async def test__exec_push31_should_add_31_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push31_should_add_31_byte_to_stack().call()
-
-    async def test__exec_push32_should_add_32_byte_to_stack(self, push_operations):
-        await push_operations.test__exec_push32_should_add_32_byte_to_stack().call()
+    @pytest.mark.parametrize("i", range(1, 33))
+    async def test__exec_push_should_push(self, push_operations, i):
+        res = await push_operations.test__exec_push_should_push(i).call()
+        assert res.result.value == int_to_uint256(256**i - 1)
