94e80ae475ff0a2aeab51db78da372fd95f95b56
Gregory Edison <gregory.edison1993@gmail.com>
2023-03-24 11:25:48 +0100

    feat: hex string to bytes array utils

---
+++ scripts/utils/utils.py
@@ -0,0 +1,9 @@
+from textwrap import wrap
+
+
+def hex_string_to_bytes_array(h: str):
+    if len(h) % 2 != 0:
+        raise ValueError(f"Provided string has an odd length {len(h)}")
+    if h[:2] == "0x":
+        h = h[2:]
+    return [int(b, 16) for b in wrap(h, 2)]
