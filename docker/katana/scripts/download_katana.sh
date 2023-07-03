7ff9876380a7c691c68137e0e9df59b40a147cf1
Harsh Bajpai <bajpaiharsh244@gmail.com>
2023-07-03 17:24:37 +0530

    fix: fix katana docker image (#615)
    Fix Katana's image, in #605 , during rebase, wrong changes were
    accepted, which ended up mixing old and new images. This PR fixes it.
    
    ## Pull request type
    
    <!-- Please try to limit your pull request to one type, submit multiple
    pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [x] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## What is the current behavior?
    
    Resolves #612 
    
    ## What is the new behavior?
    
    - The docker image should build properly on the CI

--- docker/katana/scripts/download_katana.sh
+++ docker/katana/scripts/download_katana.sh
@@ -1,9 +1,9 @@
 # Conditionally download different binaries based on architecture
 if [ "$(uname -m)" = "x86_64" ]; then \
-      wget https://github.com/dojoengine/dojo/releases/download/nightly-a319f1cf3fc8ab106c7147452a3b19b572b17f0d/dojo_nightly_linux_amd64.tar.gz
+      wget https://github.com/dojoengine/dojo/releases/download/nightly-efb3cf6841b679fd94204ace10147a8e84c5fc8a/dojo_nightly_linux_amd64.tar.gz
       tar -xvf dojo_nightly_linux_amd64.tar.gz -C /app/
     elif [ "$(uname -m)" = "aarch64" ]; then \
-      wget https://github.com/dojoengine/dojo/releases/download/nightly-a319f1cf3fc8ab106c7147452a3b19b572b17f0d/dojo_nightly_linux_arm64.tar.gz
+      wget https://github.com/dojoengine/dojo/releases/download/nightly-efb3cf6841b679fd94204ace10147a8e84c5fc8a/dojo_nightly_linux_arm64.tar.gz
       tar -xvf dojo_nightly_linux_arm64.tar.gz -C /app/
     else \
       echo "Unsupported architecture: $BUILDARCH"; \
