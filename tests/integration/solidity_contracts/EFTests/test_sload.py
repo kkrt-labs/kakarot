f247493b9fcbe27bbf72d7a849095c155d061ab0
Fred Tupas <fred.tupas21@gmail.com>
2023-09-07 10:48:04 +0200

    feat: add failing sloadGasCost_d0g0v0_Shanghai test

---
+++ tests/integration/solidity_contracts/EFTests/test_sload.py
@@ -0,0 +1,38 @@
+import logging
+
+import pytest
+from starkware.starknet.testing.contract import StarknetContract
+
+from tests.utils.uint256 import hex_string_to_uint256
+
+logger = logging.getLogger()
+
+
+@pytest.mark.asyncio
+@pytest.mark.EF_TEST
+@pytest.mark.SLOAD
+class TestSLOAD:
+    # https://github.com/kkrt-labs/ef-tests/issues/79
+    async def test_sloadGasCost_d0g0v0_Shanghai(
+        self,
+        owner,
+        create_account_with_bytecode_and_storage,
+        kakarot: StarknetContract,
+    ):
+        called_contract = await create_account_with_bytecode_and_storage(
+            "0x5a80545a905090036005900360015500",
+        )
+
+        # Send tx
+        await kakarot.eth_send_transaction(
+            to=int(called_contract.evm_contract_address, 16),
+            gas_limit=100_000_000,
+            gas_price=0,
+            value=0,
+            data=[],
+        ).execute(caller_address=owner.starknet_address)
+
+        storage_final = (
+            await called_contract.storage(hex_string_to_uint256("0x01")).call()
+        ).result.value
+        assert storage_final == hex_string_to_uint256("0x0834")
