9ff5233d515d7c895cddca4873efefbcc1edaf54
Gregory Edison <gregory.edison1993@gmail.com>
2023-09-22 13:41:38 +0700

    fix factory tests

--- tests/end_to_end/UniswapV2/test_uniswap_v2_factory.py
+++ tests/end_to_end/UniswapV2/test_uniswap_v2_factory.py
@@ -1,7 +1,5 @@
 import pytest
 
-from tests.utils.errors import kakarot_error
-
 TEST_ADDRESSES = [
     "0x1000000000000000000000000000000000000000",
     "0x2000000000000000000000000000000000000000",
@@ -12,83 +10,84 @@ TEST_ADDRESSES = [
 @pytest.mark.UniswapV2Factory
 class TestUniswapV2Factory:
     class TestDeploy:
-        async def test_should_set_constants_2(self, factory, owner):
+        async def test_should_set_constants(self, factory, owner):
             assert await factory.feeTo() == f"0x{0:040x}"
             assert await factory.feeToSetter() == owner.address
             assert await factory.allPairsLength() == 0
 
+    class TestCreatePair:
+        @pytest.mark.parametrize("tokens", [TEST_ADDRESSES, TEST_ADDRESSES[::-1]])
+        async def test_should_create_pair_only_once(
+            self,
+            factory,
+            get_solidity_contract,
+            owner,
+            tokens,
+        ):
+            receipt = await factory.createPair(*tokens, caller_eoa=owner, max_fee=0)
+            # if no events, then the pair already exists
+            # TODO: update when Katana passes the revert reason
+            if not receipt.events:
+                return
+            token_0, token_1 = sorted(tokens)
+            pair_evm_address = await factory.getPair(*tokens)
+            assert factory.events.parse_starknet_events(receipt.events)[
+                "PairCreated"
+            ] == [
+                {
+                    "token0": token_0,
+                    "token1": token_1,
+                    "pair": pair_evm_address,
+                    "": 1,
+                }
+            ]
 
-#     class TestCreatePair:
-#         @pytest.mark.parametrize("tokens", [TEST_ADDRESSES, TEST_ADDRESSES[::-1]])
-#         async def test_should_create_pair_only_once(
-#             self,
-#             factory,
-#             get_solidity_contract,
-#             get_starknet_address,
-#             owner,
-#             tokens,
-#         ):
-#             pair_evm_address = await factory.createPair(*tokens, caller_eoa=owner)
-#             token_0, token_1 = sorted(tokens)
-#             assert factory.events.PairCreated == [
-#                 {
-#                     "token0": token_0,
-#                     "token1": token_1,
-#                     "pair": pair_evm_address,
-#                     "": 1,
-#                 }
-#             ]
+            receipt = await factory.createPair(*tokens, caller_eoa=owner, max_fee=0)
+            assert not receipt.events
 
-#             # with kakarot_error("UniswapV2: PAIR_EXISTS"):
-#             #     await factory.createPair(*tokens, caller_address=owner)
+            receipt = await factory.createPair(
+                *tokens[::-1], caller_eoa=owner, max_fee=0
+            )
+            assert not receipt.events
 
-# with kakarot_error("UniswapV2: PAIR_EXISTS"):
-#     await factory.createPair(*tokens[::-1], caller_address=owner)
+            assert await factory.getPair(*tokens) == pair_evm_address
+            assert await factory.getPair(*tokens[::-1]) == pair_evm_address
+            assert await factory.allPairs(0) == pair_evm_address
+            assert await factory.allPairsLength() == 1
 
-# assert await factory.getPair(*tokens) == pair_evm_address
-# assert await factory.getPair(*tokens[::-1]) == pair_evm_address
-# assert await factory.allPairs(0) == pair_evm_address
-# assert await factory.allPairsLength() == 1
+            pair = get_solidity_contract(
+                "UniswapV2",
+                "UniswapV2Pair",
+                address=pair_evm_address,
+            )
+            assert await pair.factory() == factory.address
+            assert await pair.token0() == token_0
+            assert await pair.token1() == token_1
 
-# pair_starknet_address = get_starknet_address(int(pair_evm_address, 16))
-# pair = get_solidity_contract(
-#     "UniswapV2",
-#     "UniswapV2Pair",
-#     pair_starknet_address,
-#     pair_evm_address,
-#     None,
-# )
-# assert await pair.factory() == factory.evm_contract_address
-# assert await pair.token0() == token_0
-# assert await pair.token1() == token_1
+        @pytest.mark.skip("Skipped because gas metering is inaccurate in kakarot")
+        async def test_should_use_correct_gas(self, factory, owner):
+            await factory.createPair(*TEST_ADDRESSES, caller_eoa=owner, max_fee=0)
+            assert factory.tx.result.gas_used == 2512920
 
+    class TestSetFeeTo:
+        async def test_should_revert_when_caller_is_not_owner(self, factory, other):
+            await factory.setFeeTo(other.address, caller_eoa=other)
+            assert await factory.feeTo() != other.address
 
-#         @pytest.mark.skip("Skipped because gas metering is inaccurate in kakarot")
-#         async def test_should_use_correct_gas(self, factory, owner):
-#             await factory.createPair(*TEST_ADDRESSES, caller_address=owner)
-#             assert factory.tx.result.gas_used == 2512920
+        async def test_should_set_fee_to_owner(self, factory, owner):
+            await factory.setFeeTo(owner.address, caller_eoa=owner, max_fee=0)
+            assert await factory.feeTo() == owner.address
 
-#     class TestSetFeeTo:
-#         async def test_should_revert_when_caller_is_not_owner(self, factory, other):
-#             with kakarot_error("UniswapV2: FORBIDDEN"):
-#                 await factory.setFeeTo(other.address, caller_address=other)
+    class TestSetFeeToSetter:
+        async def test_should_revert_when_caller_is_not_owner(self, factory, other):
+            await factory.setFeeToSetter(other.address, caller_eoa=other)
+            assert await factory.feeToSetter() != other.address
 
-#         async def test_should_set_fee_to_owner(self, factory, uniswap_factory_deployer):
-#             owner = uniswap_factory_deployer
-#             await factory.setFeeTo(owner.address, caller_address=owner)
-#             assert await factory.feeTo() == owner.address
+        async def test_should_set_fee_setter_to_other_and_transfer_permission(
+            self, factory, owner, other
+        ):
+            await factory.setFeeToSetter(other.address, caller_eoa=owner)
+            assert await factory.feeToSetter() == other.address
 
-#     class TestSetFeeToSetter:
-#         async def test_should_revert_when_caller_is_not_owner(self, factory, other):
-#             with kakarot_error("UniswapV2: FORBIDDEN"):
-#                 await factory.setFeeToSetter(other.address, caller_address=other)
-
-#         async def test_should_set_fee_setter_to_other_and_transfer_permission(
-#             self, factory, uniswap_factory_deployer, other
-#         ):
-#             owner = uniswap_factory_deployer
-#             await factory.setFeeToSetter(other.address, caller_address=owner)
-#             assert await factory.feeToSetter() == other.address
-
-#             with kakarot_error("UniswapV2: FORBIDDEN"):
-#                 await factory.setFeeToSetter(owner.address, caller_address=owner)
+            await factory.setFeeToSetter(owner.address, caller_eoa=owner)
+            assert await factory.feeToSetter() != owner.address
