8884654d3fd570424c16c534f80c7ca07c23b81c
Johann <johann.bestowrous@gmail.com>
2023-09-23 16:53:48 +0700

    use keyword to filter ef-test-fixtures

--- tests/integration/ef_tests/utils.py
+++ tests/integration/ef_tests/utils.py
@@ -1,12 +1,16 @@
 import json
 import os
 
+from _pytest.mark.expression import Expression
+
 from tests.utils.uint256 import uint256_to_int
 
 # Root of the GeneralStateTest in BlockchainTest format
 EF_GENERAL_STATE_TEST_ROOT_PATH = (
     "./tests/integration/ef_tests/test_data/BlockchainTests/GeneralStateTests/"
 )
+
+DEFAULT_NETWORK = "Shanghai"
 
 
 def display_storage(uint256_tuple):
@@ -17,41 +21,41 @@ def is_account_eoa(state: dict) -> bool:
     return state.get("code") in [None, "0x"] and not state.get("storage")
 
 
-def load_json_file(filepath):
+def create_matcher(keyword):
+    expr = Expression.compile(keyword)
+    return lambda ident: expr.evaluate(lambda s: s in ident)
+
+
+def filter_by_keyword(ef_cases, keyword):
+    matcher = create_matcher(keyword)
+    return [(case_id, case_obj) for case_id, case_obj in ef_cases if matcher(case_id)]
+
+
+def process_json_file(filepath):
     with open(filepath, "r") as f:
-        return json.load(f)
-
-
-def filter_by_case_ids(ef_cases, case_ids):
-    return [
-        (case_id, case_obj) for case_id, case_obj in ef_cases if case_id in case_ids
-    ]
-
-
-def filter_network_tests(json_content, network):
+        json_content = json.load(f)
     return [
         (test_name, test_content)
         for test_name, test_content in json_content.items()
-        if test_content["network"] == network
+        if test_content["network"] == DEFAULT_NETWORK
     ]
 
 
-def load_ef_blockchain_tests_from_directory(directory_path, network):
+def load_ef_blockchain_tests_from_path(directory_path):
     ef_tests = []
-    for filename in os.listdir(directory_path):
-        filepath = os.path.join(directory_path, filename)
-
-        # Check if filepath is a directory
-        if os.path.isdir(filepath):
-            ef_tests.extend(load_ef_blockchain_tests_from_directory(filepath, network))
-        elif filename.endswith(".json"):
-            json_content = load_json_file(filepath)
-            ef_tests.extend(filter_network_tests(json_content, network))
+    if os.path.isfile(directory_path):  # Handle case where path is a file
+        ef_tests.extend(process_json_file(directory_path))
+    else:  # Handle case where path is a directory
+        for dirpath, _, filenames in os.walk(directory_path):
+            for filename in filenames:
+                if filename.endswith(".json"):
+                    filepath = os.path.join(dirpath, filename)
+                    ef_tests.extend(process_json_file(filepath))
 
     return ef_tests
 
 
-def load_ef_blockchain_tests(relative_path, network):
+def _load_ef_blockchain_tests(relative_path):
     """
     Load Ethereum Foundation tests from the directory or file specified
     under a fixed root path.
@@ -60,20 +64,10 @@ def load_ef_blockchain_tests(relative_path, network):
     if not os.path.exists(full_path):
         raise ValueError(f"The path {full_path} does not exist.")
 
-    if full_path.endswith(".json"):
-        if not os.path.isfile(full_path):
-            raise ValueError(f"The file {full_path} does not exist.")
-        json_content = load_json_file(full_path)
-        return filter_network_tests(json_content, network)
-
-    elif os.path.isdir(full_path):
-        return load_ef_blockchain_tests_from_directory(full_path, network)
-
-    else:
-        raise ValueError(
-            f"Invalid full_path: {full_path}. It's neither a directory nor a .json file."
-        )
+    return load_ef_blockchain_tests_from_path(full_path)
 
 
-def load_default_ef_blockchain_tests(network_name):
-    return {}
+def load_ef_blockchain_tests(keyword):
+    # Load all available tests and filter them using the keyword
+    all_tests = _load_ef_blockchain_tests(".")
+    return filter_by_keyword(all_tests, keyword)
