a5fe31c6bdf3e2cf886f115937f88082d45d11a3
Johann <johann.bestowrous@gmail.com>
2023-09-28 09:05:32 +0700

    give warning/guidance when submodule isn't pulled

--- tests/integration/ef_tests/conftest.py
+++ tests/integration/ef_tests/conftest.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 from pathlib import Path
 
@@ -9,13 +10,26 @@ EF_GENERAL_STATE_TEST_ROOT_PATH = Path(
 
 DEFAULT_NETWORK = "Shanghai"
 
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 
 def pytest_generate_tests(metafunc):
     """
-    Parameterizes `ef_blockchain_test` fixture with cases loaded from the Ethereum Foundation tests repository,
-    see: https://github.com/kkrt-labs/kakarot/blob/main/.gitmodules#L7.
+    Parameterizes `ef_blockchain_test` fixture with cases loaded from the
+    Ethereum Foundation tests repository, see:
+    https://github.com/kkrt-labs/kakarot/blob/main/.gitmodules#L7.
     """
     if "ef_blockchain_test" not in metafunc.fixturenames:
+        return
+
+    if not EF_GENERAL_STATE_TEST_ROOT_PATH.exists():
+        logger.warning(
+            "EFTests directory %s doesn't exist. Run `make pull-ef-tests`",
+            str(EF_GENERAL_STATE_TEST_ROOT_PATH),
+        )
+        metafunc.parametrize("ef_blockchain_test", [])
         return
 
     test_ids, test_cases = zip(
