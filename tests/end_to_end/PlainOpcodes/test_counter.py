390b4d5d9ffe67e498e3a34d68ef714993ac85dc
Clément Walter <clement0walter@gmail.com>
2023-10-10 21:57:48 +0200

    Fix web3 doesn't handle evm_address as int with leading 0 in hex form

--- tests/end_to_end/PlainOpcodes/test_counter.py
+++ tests/end_to_end/PlainOpcodes/test_counter.py
@@ -21,47 +21,45 @@ class TestCounter:
 
     class TestInc:
         async def test_should_increase_count(self, counter, owner):
-            count_before = await counter.count()
+            await counter.reset(caller_eoa=owner)
             await counter.inc(caller_eoa=owner)
-            count_after = await counter.count()
-            assert count_after - count_before == 1
+            assert await counter.count() == 1
 
     class TestDec:
         @pytest.mark.xfail(
             os.environ.get("STARKNET_NETWORK", "katana") == "katana",
             reason="https://github.com/dojoengine/dojo/issues/864",
         )
-        async def test_should_raise_when_count_is_0_from_modifier(self, counter, owner):
+        async def test_should_raise_from_modifier_when_count_is_0(self, counter, owner):
+            await counter.reset(caller_eoa=owner)
             with kakarot_error("count should be strictly greater than 0"):
                 await counter.dec(caller_eoa=owner)
 
         @pytest.mark.xfail(
             reason="https://github.com/kkrt-labs/kakarot/issues/683",
         )
-        async def test_should_raise_when_count_is_0_from_vm(self, counter, owner):
+        async def test_should_raise_from_vm_when_count_is_0(self, counter, owner):
+            await counter.reset(caller_eoa=owner)
             with kakarot_error():
                 await counter.decUnchecked(caller_eoa=owner)
 
         async def test_should_decrease_count(self, counter, owner):
-            count_before = await counter.count()
+            await counter.reset(caller_eoa=owner)
             await counter.inc(caller_eoa=owner)
             await counter.dec(caller_eoa=owner)
-            count_after = await counter.count()
-            assert count_after == count_before
+            assert await counter.count() == 0
 
         async def test_should_decrease_count_unchecked(self, counter, owner):
-            count_before = await counter.count()
+            await counter.reset(caller_eoa=owner)
             await counter.inc(caller_eoa=owner)
             await counter.decUnchecked(caller_eoa=owner)
-            count_after = await counter.count()
-            assert count_after == count_before
+            assert await counter.count() == 0
 
         async def test_should_decrease_count_in_place(self, counter, owner):
-            count_before = await counter.count()
+            await counter.reset(caller_eoa=owner)
             await counter.inc(caller_eoa=owner)
             await counter.decInPlace(caller_eoa=owner)
-            count_after = await counter.count()
-            assert count_after == count_before
+            assert await counter.count() == 0
 
     class TestReset:
         async def test_should_set_count_to_0(self, counter, owner):
