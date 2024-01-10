bdb2a3220125334da08153ec2408c9f9dccac896
enitrat <msaug@protonmail.com>
2024-01-10 17:41:44 +0100

    fix: trunk check

--- tests/src/kakarot/test_execution_context.py
+++ tests/src/kakarot/test_execution_context.py
@@ -18,11 +18,16 @@ class TestExecutionContext:
     @pytest.mark.parametrize(
         "bytecode, jumpdest, new_pc, expected_return_data",
         [
-            ([0,0x5B], 0, 0, list(b"Kakarot: invalidJumpDestError")), # not 0x5b
-            ([0,0x5B], 2, 0, list(b"Kakarot: invalidJumpDestError")), # out of bounds
-            ([0,0x5B], 1, 1, []),
-            ([0,0x60, 0x01, 0x5B], 3, 3, []), # post-push1 opcode
-            ([0,0x61, 0x5B, 0x02], 2, 0, list(b"Kakarot: invalidJumpDestError")), # post-push2 opcode
+            ([0, 0x5B], 0, 0, list(b"Kakarot: invalidJumpDestError")),  # not 0x5b
+            ([0, 0x5B], 2, 0, list(b"Kakarot: invalidJumpDestError")),  # out of bounds
+            ([0, 0x5B], 1, 1, []),
+            ([0, 0x60, 0x01, 0x5B], 3, 3, []),  # post-push1 opcode
+            (
+                [0, 0x61, 0x5B, 0x02],
+                2,
+                0,
+                list(b"Kakarot: invalidJumpDestError"),
+            ),  # post-push2 opcode
         ],
     )
     async def test_jump(
