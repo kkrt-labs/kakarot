8add3d8164ee76ba98e62cc9e55b3845385a8359
Clément Walter <clement0walter@gmail.com>
2024-01-10 18:09:13 +0100

    Refacto test_uint256

--- tests/src/utils/test_uint256.py
+++ tests/src/utils/test_uint256.py
@@ -1,28 +1,25 @@
-import pytest
-import pytest_asyncio
-from starkware.starknet.testing.starknet import Starknet
+from pathlib import Path
 
+import pytest
+from starkware.cairo.lang.cairo_constants import DEFAULT_PRIME
+from starkware.cairo.lang.compiler.cairo_compile import compile_cairo
+
+from tests.utils.cairo import run_program_entrypoint
 from tests.utils.uint256 import int_to_uint256
 
 
-@pytest_asyncio.fixture(scope="module")
-async def uint256(starknet: Starknet):
-    class_hash = await starknet.deprecated_declare(
-        source="./tests/src/utils/test_uint256.cairo",
-        cairo_path=["src"],
-        disable_hint_validation=True,
-    )
-    return await starknet.deploy(class_hash=class_hash.class_hash)
+@pytest.fixture(scope="module")
+def program():
+    path = Path("tests/src/utils/test_uint256.cairo")
+    return compile_cairo(path.read_text(), cairo_path=["src"], prime=DEFAULT_PRIME)
 
 
-@pytest.mark.asyncio
 class TestUint256:
     class TestUint256ToUint160:
         @pytest.mark.parametrize("n", [0, 2**128, 2**160 - 1, 2**160, 2**256])
-        async def test_should_cast_value(self, uint256, n):
-            assert (
-                n % 2**160
-                == (
-                    await uint256.test__uint256_to_uint160(int_to_uint256(n)).call()
-                ).result.uint160
+        def test_should_cast_value(self, program, n):
+            run_program_entrypoint(
+                program,
+                "test__uint256_to_uint160",
+                {"x": int_to_uint256(n), "expected": n % 2**160},
             )
