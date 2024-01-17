050efb07ef2f1b83492d3ae35a13e7bab2add84d
enitrat <msaug@protonmail.com>
2024-01-17 15:29:09 +0100

    fix: memory cost calculation

--- tests/src/kakarot/test_gas.cairo
+++ tests/src/kakarot/test_gas.cairo
@@ -1,11 +1,48 @@
 %builtins range_check
 
 from kakarot.gas import Gas
+from starkware.cairo.common.uint256 import Uint256
 
 func test__memory_cost{range_check_ptr}() {
     tempvar words_len: felt;
     %{ ids.words_len = program_input["words_len"]; %}
     let cost = Gas.memory_cost(words_len);
+
+    %{ segments.write_arg(output, [ids.cost]); %}
+    return ();
+}
+
+func test__memory_expansion_cost{range_check_ptr}() {
+    tempvar words_len: felt;
+    tempvar max_offset: felt;
+    %{
+        ids.words_len = program_input["words_len"];
+        ids.max_offset = program_input["max_offset"];
+    %}
+    let cost = Gas.memory_expansion_cost(words_len, max_offset);
+
+    %{ segments.write_arg(output, [ids.cost]); %}
+    return ();
+}
+
+func test__max_memory_expansion_cost{range_check_ptr}() {
+    tempvar words_len: felt;
+    tempvar offset_1: Uint256;
+    tempvar size_1: Uint256;
+    tempvar offset_2: Uint256;
+    tempvar size_2: Uint256;
+    %{
+        ids.words_len = program_input["words_len"];
+        ids.offset_1.low = program_input["offset_1"];
+        ids.offset_1.high = 0;
+        ids.size_1.low = program_input["size_1"];
+        ids.size_1.high = 0;
+        ids.offset_2.low = program_input["offset_2"];
+        ids.offset_2.high = 0;
+        ids.size_2.low = program_input["size_2"];
+        ids.size_2.high = 0;
+    %}
+    let cost = Gas.max_memory_expansion_cost(words_len, offset_1, size_1, offset_2, size_2);
 
     %{ segments.write_arg(output, [ids.cost]); %}
     return ();
--- tests/src/kakarot/test_gas.py
+++ tests/src/kakarot/test_gas.py
@@ -1,10 +1,74 @@
+import random
+
 import pytest
-from ethereum.shanghai.vm.gas import calculate_memory_gas_cost
+from ethereum.shanghai.vm.gas import (
+    calculate_gas_extend_memory,
+    calculate_memory_gas_cost,
+)
 
 
 class TestGas:
     class TestCost:
-        @pytest.mark.parametrize("max_offset", [0, 0xFF, 0xFFFF, 0xFFFFFF, 0xFFFFFFFF])
+        @pytest.mark.parametrize(
+            "max_offset",
+            [random.randint(0, 0xFFFFFF) for _ in range(100)],
+        )
         def test_should_return_same_as_execution_specs(self, cairo_run, max_offset):
             output = cairo_run("test__memory_cost", words_len=((max_offset + 31) // 32))
             assert calculate_memory_gas_cost(max_offset) == output[0]
+
+        @pytest.mark.parametrize(
+            "bytes_len", [random.randint(0, 0xFFFFFF) for _ in range(5)]
+        )
+        @pytest.mark.parametrize(
+            "added_offset", [random.randint(0, 0xFFFFFF) for _ in range(5)]
+        )
+        def test_should_return_correct_expansion_cost(
+            self, cairo_run, bytes_len, added_offset
+        ):
+            cost_before = calculate_memory_gas_cost(bytes_len)
+            cost_after = calculate_memory_gas_cost(bytes_len + added_offset)
+            diff = cost_after - cost_before
+
+            words_len = (bytes_len + 31) // 32
+            max_offset = bytes_len + added_offset
+            output = cairo_run(
+                "test__memory_expansion_cost",
+                words_len=words_len,
+                max_offset=max_offset,
+            )
+            assert diff == output[0]
+
+        @pytest.mark.parametrize(
+            "offset_1", [random.randint(0, 0xFFFF) for _ in range(3)]
+        )
+        @pytest.mark.parametrize(
+            "size_1", [random.randint(0, 0xFFFF) for _ in range(3)]
+        )
+        @pytest.mark.parametrize(
+            "offset_2", [random.randint(0, 0xFFFF) for _ in range(3)]
+        )
+        @pytest.mark.parametrize(
+            "size_2", [random.randint(0, 0xFFFF) for _ in range(3)]
+        )
+        def test_should_return_max_expansion_cost(
+            self, cairo_run, offset_1, size_1, offset_2, size_2
+        ):
+            output = cairo_run(
+                "test__max_memory_expansion_cost",
+                words_len=0,
+                offset_1=offset_1,
+                size_1=size_1,
+                offset_2=offset_2,
+                size_2=size_2,
+            )
+            assert (
+                output[0]
+                == calculate_gas_extend_memory(
+                    b"",
+                    [
+                        (offset_1, size_1),
+                        (offset_2, size_2),
+                    ],
+                ).cost
+            )
