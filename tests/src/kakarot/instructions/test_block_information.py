aed38bf43eab6c321e30d2e518787abdff2e0568
Clément Walter <clement0walter@gmail.com>
2024-01-22 15:12:38 +0100

    Update test_block_information

--- tests/src/kakarot/instructions/test_block_information.py
+++ tests/src/kakarot/instructions/test_block_information.py
@@ -1,47 +1,25 @@
 import pytest
-import pytest_asyncio
-from starkware.starknet.testing.starknet import Starknet
 
-from tests.utils.constants import (
-    BLOCK_GAS_LIMIT,
-    BLOCK_NUMBER,
-    BLOCK_TIMESTAMP,
-    CHAIN_ID,
-    Opcodes,
-)
+from tests.utils.constants import BLOCK_GAS_LIMIT, CHAIN_ID, Opcodes
+from tests.utils.syscall_handler import SyscallHandler
 
 
-@pytest_asyncio.fixture(scope="module")
-async def block_information(starknet: Starknet):
-    class_hash = await starknet.deprecated_declare(
-        source="./tests/src/kakarot/instructions/test_block_information.cairo",
-        cairo_path=["src"],
-        disable_hint_validation=True,
-    )
-    return await starknet.deploy(class_hash=class_hash.class_hash)
-
-
-@pytest.mark.asyncio
 class TestBlockInformation:
     @pytest.mark.parametrize(
         "opcode,expected_result",
         [
             (
                 Opcodes.COINBASE,
-                (0xACDFFE0CF08E20ED8BA10EA97A487004, 0x388CA486B82E20CC81965D056B4CDCA),
+                [0xACDFFE0CF08E20ED8BA10EA97A487004, 0x388CA486B82E20CC81965D056B4CDCA],
             ),
-            (Opcodes.TIMESTAMP, (BLOCK_TIMESTAMP, 0)),
-            (Opcodes.NUMBER, (BLOCK_NUMBER, 0)),
-            (Opcodes.PREVRANDAO, (0, 0)),
-            (Opcodes.GASLIMIT, (BLOCK_GAS_LIMIT, 0)),
-            (Opcodes.CHAINID, (CHAIN_ID, 0)),
-            (Opcodes.BASEFEE, (0, 0)),
+            (Opcodes.TIMESTAMP, [SyscallHandler.block_timestamp, 0]),
+            (Opcodes.NUMBER, [SyscallHandler.block_number, 0]),
+            (Opcodes.PREVRANDAO, [0, 0]),
+            (Opcodes.GASLIMIT, [BLOCK_GAS_LIMIT, 0]),
+            (Opcodes.CHAINID, [CHAIN_ID, 0]),
+            (Opcodes.BASEFEE, [0, 0]),
         ],
     )
-    async def test__exec_block_information(
-        self, block_information, opcode, expected_result
-    ):
-        (result,) = (
-            await block_information.test__exec_block_information(opcode, []).call()
-        ).result
-        assert result == expected_result
+    def test__exec_block_information(self, cairo_run, opcode, expected_result):
+        output = cairo_run("test__exec_block_information", opcode=opcode)
+        assert output == expected_result
