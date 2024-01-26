683d732d88ad2dbf415b0bcfc194a3a9a0da01c1
Clément Walter <clement0walter@gmail.com>
2024-01-26 14:28:16 +0100

    Refacto ec_recover

--- tests/src/kakarot/precompiles/test_ec_recover.py
+++ tests/src/kakarot/precompiles/test_ec_recover.py
@@ -1,37 +1,20 @@
 import pytest
-import pytest_asyncio
-from starkware.starknet.testing.starknet import Starknet
 
 
-@pytest_asyncio.fixture(scope="module")
-async def ec_recover(starknet: Starknet):
-    class_hash = await starknet.deprecated_declare(
-        source="./tests/src/kakarot/precompiles/test_ec_recover.cairo",
-        cairo_path=["src"],
-        disable_hint_validation=True,
-    )
-    return await starknet.deploy(class_hash=class_hash.class_hash)
-
-
-@pytest.mark.asyncio
 @pytest.mark.EC_RECOVER
 class TestEcRecover:
-    async def test_should_fail_when_input_len_is_not_128(self, ec_recover):
-        (output,) = (
-            await ec_recover.test_should_fail_when_input_len_is_not_128().call()
-        ).result
-        assert bytes(output).decode() == "Precompile: wrong input_len"
+    def test_should_fail_when_input_len_is_not_128(self, cairo_run):
+        output = cairo_run("test_should_fail_when_input_len_is_not_128")
+        assert bytes(output) == b"Precompile: wrong input_len"
 
-    async def test_should_fail_when_recovery_identifier_is_neither_27_nor_28(
-        self, ec_recover
-    ):
-        (output,) = (
-            await ec_recover.test_should_fail_when_recovery_identifier_is_neither_27_nor_28().call()
-        ).result
-        assert bytes(output).decode() == "Precompile: flag error"
+    def test_should_fail_when_recovery_identifier_is_neither_27_nor_28(self, cairo_run):
+        output = cairo_run(
+            "test_should_fail_when_recovery_identifier_is_neither_27_nor_28"
+        )
+        assert bytes(output) == b"Precompile: flag error"
 
-    async def test_should_return_evm_address_in_bytes32(self, ec_recover):
-        await ec_recover.test_should_return_evm_address_in_bytes32().call()
+    def test_should_return_evm_address_in_bytes32(self, cairo_run):
+        cairo_run("test_should_return_evm_address_in_bytes32")
 
-    async def test_should_return_evm_address_for_playground_example(self, ec_recover):
-        await ec_recover.test_should_return_evm_address_for_playground_example().call()
+    def test_should_return_evm_address_for_playground_example(self, cairo_run):
+        cairo_run("test_should_return_evm_address_for_playground_example")
