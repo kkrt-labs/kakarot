3099112b5033912211b769d3dc3ddeff6fdac113
Gregory Edison <gregory.edison1993@gmail.com>
2024-03-28 19:40:23 +0100

    add sepolia to the list of testnets

--- kakarot_scripts/utils/starknet.py
+++ kakarot_scripts/utils/starknet.py
@@ -103,6 +103,7 @@ async def get_starknet_account(
                 or "Client failed with code 21: Invalid message selector." in message
                 or "StarknetErrorCode.ENTRY_POINT_NOT_FOUND_IN_CONTRACT" in message
                 or ("code 40" in message and "not found in contract" in message)
+                or "Invalid message selector"
             ):
                 continue
             else:
