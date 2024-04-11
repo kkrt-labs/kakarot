65cc7fa55b61e9ada156afbacc42012c8d81f442
enitrat <msaug@protonmail.com>
2024-04-11 22:06:02 +0200

    fix: tests

--- tests/end_to_end/UniswapV2/conftest.py
+++ tests/end_to_end/UniswapV2/conftest.py
@@ -3,7 +3,7 @@ import pytest_asyncio
 TOTAL_SUPPLY = 10000 * 10**18
 
 
-@pytest_asyncio.fixture(scope="module")
+@pytest_asyncio.fixture(scope="function")
 async def token_a(deploy_solidity_contract, owner):
     return await deploy_solidity_contract(
         "UniswapV2",
@@ -13,7 +13,7 @@ async def token_a(deploy_solidity_contract, owner):
     )
 
 
-@pytest_asyncio.fixture(scope="module")
+@pytest_asyncio.fixture(scope="function")
 async def token_b(deploy_solidity_contract, owner):
     return await deploy_solidity_contract(
         "UniswapV2",
