5612cd034a62a82133204cc6fad9e794ccebb5c0
Sweta Shaw <swt.shw94@gmail.com>
2024-04-17 17:49:05 +0530

    format

--- vyper_contracts/scripts/compile.py
+++ vyper_contracts/scripts/compile.py
@@ -1,6 +1,7 @@
-import subprocess
 import json
 import os
+import subprocess
+
 
 def compile_vyper_contract(filename, output_dir):
     # Prepare commands for compiling the contract to bytecode and ABI
@@ -14,19 +15,16 @@ def compile_vyper_contract(filename, output_dir):
     # Check if both compilations were successful
     if bytecode_result.returncode == 0 and abi_result.returncode == 0:
         print(f"Compilation successful for {filename}, writing artifacts.")
-        
+
         # Parse the ABI JSON string
         abi_data = json.loads(abi_result.stdout)
-        
+
         # Create a dictionary with both bytecode and ABI
-        data = {
-            "bytecode": bytecode_result.stdout.strip(),
-            "abi": abi_data
-        }
+        data = {"bytecode": bytecode_result.stdout.strip(), "abi": abi_data}
 
         # Construct the output JSON filename based on the contract's filename
         basename = os.path.basename(filename)
-        json_filename = os.path.splitext(basename)[0] + '.json'
+        json_filename = os.path.splitext(basename)[0] + ".json"
 
         # Write the data to a JSON file in the specified output directory
         with open(os.path.join(output_dir, json_filename), "w") as file:
@@ -38,6 +36,7 @@ def compile_vyper_contract(filename, output_dir):
         if abi_result.returncode != 0:
             print(abi_result.stderr)
 
+
 def compile_all_contracts(source_dir, build_dir):
     # Ensure the build directory exists
     os.makedirs(build_dir, exist_ok=True)
@@ -47,9 +46,14 @@ def compile_all_contracts(source_dir, build_dir):
         if item.endswith(".vy"):
             compile_vyper_contract(os.path.join(source_dir, item), build_dir)
 
+
 if __name__ == "__main__":
-    source_directory = "vyper_contracts/src"  # The directory where vyper contracts are located
-    build_directory = "vyper_contracts/build"       # The directory where compiled files will be saved
+    source_directory = (
+        "vyper_contracts/src"  # The directory where vyper contracts are located
+    )
+    build_directory = (
+        "vyper_contracts/build"  # The directory where compiled files will be saved
+    )
 
     # Compile all contracts in the source directory
     compile_all_contracts(source_directory, build_directory)
