90b85b0ffc0b72b4012733eb23b3a9b42bc4889d
enitrat <msaug@protonmail.com>
2024-04-17 18:52:59 +0200

    fix: test

--- tests/end_to_end/test_account.py
+++ tests/end_to_end/test_account.py
@@ -1,7 +1,6 @@
 import pytest
 import pytest_asyncio
 from starknet_py.net.full_node_client import FullNodeClient
-from starkware.starknet.public.abi import get_storage_var_address
 
 TOTAL_SUPPLY = 10000 * 10**18
 TEST_AMOUNT = 10 * 10**18
@@ -83,34 +82,3 @@ class TestAccount:
                 new_account.starknet_contract.address
             )
             assert new_class == target_class
-
-        async def test_should_update_cairo1_helpers_class(
-            self,
-            starknet: FullNodeClient,
-            invoke,
-            counter,
-            new_account,
-            class_hashes,
-        ):
-            prev_cairo1_helpers_class = await starknet.get_storage_at(
-                new_account.starknet_contract.address,
-                get_storage_var_address("Account_cairo1_helpers_class_hash"),
-            )
-            target_class = class_hashes["Cairo1HelpersFixture"]
-            assert prev_cairo1_helpers_class != target_class
-            assert prev_cairo1_helpers_class == class_hashes["Cairo1Helpers"]
-
-            await invoke(
-                "kakarot",
-                "set_account_contract_class_hash",
-                class_hashes["account_contract_fixture"],
-            )
-            await invoke("kakarot", "set_cairo1_helpers_class_hash", target_class)
-
-            await assert_counter_transaction_success(counter, new_account)
-
-            new_cairo1_helpers_class = await starknet.get_storage_at(
-                new_account.starknet_contract.address,
-                get_storage_var_address("Account_cairo1_helpers_class_hash"),
-            )
-            assert new_cairo1_helpers_class == target_class
