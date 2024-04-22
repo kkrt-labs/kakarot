8484ba27edd49891233ab21cb96851e1660f681e
Elias Tazartes <elias.tazartes@gmail.com>
2024-04-22 12:48:18 +0200

    fix events

--- kakarot_scripts/utils/kakarot.py
+++ kakarot_scripts/utils/kakarot.py
@@ -380,7 +380,8 @@ async def eth_send_transaction(
     transaction_events = [
         event
         for event in receipt.events
-        if event.from_address == evm_account.address
+        # TODO: from_address=0 in new katana version, no reason why
+        if event.from_address in [evm_account.address, 0]
         and event.keys[0] == starknet_keccak(b"transaction_executed")
     ]
     if len(transaction_events) != 1:
