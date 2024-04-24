011b632a9eb753a031db4614e5927314b9ab61a4
Elias Tazartes <elias.tazartes@gmail.com>
2024-04-24 17:55:18 +0200

    add liquidity test

--- tests/end_to_end/UniswapV2/conftest.py
+++ tests/end_to_end/UniswapV2/conftest.py
@@ -13,12 +13,11 @@ async def token_a(deploy_contract, owner):
     )
 
 
-@pytest_asyncio.fixture(scope="function")
-async def token_b(deploy_contract, owner):
+@pytest_asyncio.fixture(scope="module")
+async def weth(deploy_contract, owner):
     return await deploy_contract(
-        "UniswapV2",
-        "ERC20",
-        TOTAL_SUPPLY,
+        "WETH",
+        "WETH9",
         caller_eoa=owner.starknet_contract,
     )
 
@@ -32,5 +31,21 @@ async def factory(
         "UniswapV2",
         "UniswapV2Factory",
         owner.address,
+        caller_eoa=owner.starknet_contract,
+    )
+
+
+@pytest_asyncio.fixture(scope="module")
+async def router(
+    deploy_contract,
+    owner,
+    factory,
+    weth,
+):
+    return await deploy_contract(
+        "UniswapV2Router",
+        "UniswapV2Router02",
+        factory.address,
+        weth.address,
         caller_eoa=owner.starknet_contract,
     )
---
+++ tests/end_to_end/UniswapV2/test_uniswap_v2_router.py
@@ -0,0 +1,64 @@
+import pytest
+
+
+@pytest.mark.asyncio(scope="session")
+@pytest.mark.UniswapV2Router
+class TestUniswapV2Router:
+    class TestDeploy:
+        async def test_should_set_constants(self, router, weth, factory):
+            assert await router.WETH() == weth.address
+            assert await router.factory() == factory.address
+
+    class TestAddLiquidity:
+        async def test_should_add_liquidity(
+            self, router, token_a, deploy_contract, owner
+        ):
+            token_b = await deploy_contract(
+                "UniswapV2",
+                "ERC20",
+                10000 * 10**18,
+                caller_eoa=owner.starknet_contract,
+            )
+
+            amount_A_desired = (
+                1000 * await token_a.decimals()
+            )  # This needs to match the token's decimals
+            amount_B_desired = (
+                500 * await token_b.decimals()
+            )  # Assuming WETH has 18 decimals
+
+            amount_A_min = 0
+            amount_B_min = 0
+
+            deadline = 99999999999
+
+            to_address = owner.address
+
+            await token_a.mint(
+                owner.address, amount_A_desired, caller_eoa=owner.starknet_contract
+            )
+            await token_b.mint(
+                owner.address, amount_B_desired, caller_eoa=owner.starknet_contract
+            )
+
+            await token_a.approve(
+                router.address, amount_A_desired * 2, caller_eoa=owner.starknet_contract
+            )
+            await token_b.approve(
+                router.address, amount_B_desired * 2, caller_eoa=owner.starknet_contract
+            )
+
+            success = (
+                await router.addLiquidity(
+                    token_a.address,
+                    token_b.address,
+                    amount_A_desired,
+                    amount_B_desired,
+                    amount_A_min,
+                    amount_B_min,
+                    to_address,
+                    deadline,
+                    caller_eoa=owner.starknet_contract,
+                )
+            )["success"]
+            assert success == 1
