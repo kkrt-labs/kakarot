96645dd88d0a3bb12a8524b25b0059298349fada
Danilo Woo Hyung Kim <danilowoohyungkim@Danilos-MacBook-Pro-2.local>
2024-04-25 10:49:18 -0300

    add liquidity test

--- tests/end_to_end/UniswapV2/test_uniswap_v2_router.py
+++ tests/end_to_end/UniswapV2/test_uniswap_v2_router.py
@@ -2,24 +2,44 @@ import pytest
 
 
 @pytest.mark.asyncio(scope="session")
-class TestUniswapV2Router:
+@pytest.mark.UniswapV2Router
+class TestUniswapV2Router:        
     class TestDeploy:
-        async def test_should_set_constants(self, deploy_solidity_contract):
-            from eth_keys import keys
+        async def test_should_set_constants(self, router, token_WETH, factory):
+            assert await router.WETH() == token_WETH.address
+            assert await router.factory() == factory.address
 
-            from kakarot_scripts.utils.kakarot import get_eoa
+    class TestAddLiquidity:
+        async def test_should_add_liquidity(self, router, token_b, token_a, owner):
+            # Set the desired amounts of token_WETH and token_a to be added to the pool
+            amount_A_desired = 1000 * 10**18  # This needs to match the token's decimals
+            amount_B_desired = 500 * 10**18   # Assuming WETH has 18 decimals
 
-            return await deploy_solidity_contract(
-                "UniswapV2Router",
-                "UniswapV2Router02",
-                "0x0000000000000000000000000000000000000000",
-                "0x0000000000000000000000000000000000000000",
-                caller_eoa=await get_eoa(
-                    keys.PrivateKey(
-                        bytes.fromhex(
-                            "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-                        )
-                    ),
-                    0,
-                ),
-            )
+            amount_A_min = 0
+            amount_B_min = 0
+
+            deadline = 99999999999  # Use an appropriate value for testing
+
+            to_address = owner.address
+
+            await token_a.approve(router.address, amount_A_desired, caller_eoa=owner.starknet_contract)
+            await token_b.approve(router.address, amount_B_desired, caller_eoa=owner.starknet_contract)
+
+            (amountA, amountB, liquidity) = (
+                await router.addLiquidity(
+                    token_a.address,
+                    token_b.address,
+                    amount_A_desired,
+                    amount_B_desired,
+                    amount_A_min,
+                    amount_B_min,
+                    to_address,
+                    deadline,
+                    caller_eoa=owner.starknet_contract
+                )
+            )["return_value"]
+
+            # Check the returned liquidity tokens are greater than 0
+            assert liquidity > 0
+
+            # Additional assertions can check balances of tokens and pool state if necessary
