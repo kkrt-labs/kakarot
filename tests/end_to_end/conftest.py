468c7764c6650a3c4126e4b649ca8e4ab9ab4cb3
enitrat <msaug@protonmail.com>
2024-04-29 10:40:29 +0200

    fix rebase issue

--- tests/end_to_end/conftest.py
+++ tests/end_to_end/conftest.py
@@ -246,7 +246,7 @@ def eth_balance_of(eth: Contract, compute_starknet_address):
 
 
 @pytest.fixture(scope="session")
-def deploy_contract(zero_fee: int):
+def deploy_contract(default_fee: int):
     """
     Fixture to attach a modified web3.contract instance to an already deployed contract_account in kakarot.
     """
