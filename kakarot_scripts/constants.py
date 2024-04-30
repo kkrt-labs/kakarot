56bde47d6ff227caf0362e3055a64e5cc7a4dc52
enitrat <msaug@protonmail.com>
2024-04-30 09:37:24 +0200

    bump max wait time

--- kakarot_scripts/constants.py
+++ kakarot_scripts/constants.py
@@ -59,7 +59,7 @@ NETWORKS = {
         "rpc_url": os.getenv("KATANA_RPC_URL", "http://127.0.0.1:5050"),
         "devnet": True,
         "check_interval": 0.01,
-        "max_wait": 1,
+        "max_wait": 2,
     },
     "madara": {
         "name": "madara",
