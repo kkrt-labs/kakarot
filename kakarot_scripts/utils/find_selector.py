3986c94959583c99bacd835f12790db46a9a6a2e
enitrat <msaug@protonmail.com>
2024-05-04 11:23:04 +0200

    improve find_selector script

--- kakarot_scripts/utils/find_selector.py
+++ kakarot_scripts/utils/find_selector.py
@@ -5,45 +5,28 @@ from starkware.starknet.public.abi import get_selector_from_name
 
 
 def find_cairo_functions(directory):
-    function_names = []
-
-    for root, _, files in os.walk(directory):
-        for file in files:
-            if file.endswith(".cairo"):
-                file_path = os.path.join(root, file)
-                with open(file_path, "r") as f:
-                    content = f.read()
-                    matches = re.findall(r"func\s+(\w+)\(", content)
-                    function_names.extend(matches)
-
-    return function_names
+    return [
+        match
+        for root, _, files in os.walk(directory)
+        for file in files
+        if file.endswith(".cairo")
+        for match in re.findall(
+            r"func\s+(\w+)\(", open(os.path.join(root, file)).read()
+        )
+    ]
 
 
 def map_selectors(functions):
-    selectors = {}
-    for function in functions:
-        selector = get_selector_from_name(function)
-        if selector not in selectors:
-            selectors[selector] = function
-    return selectors
+    return {get_selector_from_name(function): function for function in functions}
 
 
 def get_function_from_selector(selectors):
-    selector = int(input("Enter the selector: "))
-    if selector in selectors:
-        function_name = selectors[selector]
-        print(f"Function corresponding to selector {selector}: {function_name}")
-    else:
-        print(f"No function found for selector {selector}")
+    selector = int(input("Enter the hexadecimal selector: "), 16)
+    print(f"Corresponding function: {selectors.get(selector, 'Not found')}")
 
 
-# Directory to start the search from
-directory = "."
-
-# Find all the function names in .cairo files
-functions = find_cairo_functions(directory)
-selectors = map_selectors(functions)
-
-
-# Get the function name based on user input
-get_function_from_selector(selectors)
+if __name__ == "__main__":
+    directory = "."
+    functions = find_cairo_functions(directory)
+    selectors = map_selectors(functions)
+    get_function_from_selector(selectors)
