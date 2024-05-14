777b351696a30c62464931f3ef0221c024c2941e
enitrat <msaug@protonmail.com>
2024-05-14 12:13:28 +0200

    address review

--- tests/end_to_end/RIP7212/test_rip_7212.py
+++ tests/end_to_end/RIP7212/test_rip_7212.py
@@ -1,3 +1,5 @@
+from textwrap import wrap
+
 import pytest
 import pytest_asyncio
 
@@ -18,10 +20,14 @@ class TestEIP3074:
         async def test_should_execute_authorized_call(self, p256_verify_invoker, owner):
 
             # input data from <https://github.com/ulerdogan/go-ethereum/blob/75062a6998e4e3fbb4cdb623b8b02e79e7b8f965/core/vm/contracts_test.go#L401-L410>
-            input_data = "4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e"
-            msg_hash, r_, s_, x_, y_ = [
-                f"0x{input_data[i : i + 64]}" for i in range(0, len(input_data), 64)
-            ]
+            input_data = (
+                "4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4d",
+                "a73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac",
+                "36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d60",
+                "4aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff3",
+                "7618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e",
+            )
+            msg_hash, r_, s_, x_, y_ = [["0x" + word for word in wrap(input_data, 64)]]
             is_valid = await p256_verify_invoker.p256verify(
                 msg_hash,
                 r_,
--- tests/src/kakarot/precompiles/test_precompiles.py
+++ tests/src/kakarot/precompiles/test_precompiles.py
@@ -28,46 +28,32 @@ class TestPrecompiles:
 
         # input data from <https://github.com/ulerdogan/go-ethereum/blob/75062a6998e4e3fbb4cdb623b8b02e79e7b8f965/core/vm/contracts_test.go#L401-L410>
         @pytest.mark.parametrize(
-            "address, input_data",
+            "address, input_data, expected_return_data",
             [
                 (
                     0x100,
                     bytes.fromhex(
                         "4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e"
                     ),
-                )
-            ],
-        )
-        def test__p256_verify_precompile_should_succeed(
-            self, cairo_run, address, input_data
-        ):
-            return_data, reverted, gas_used = cairo_run(
-                "test__precompiles_run", address=address, input=input_data
-            )
-            assert not reverted
-            assert len(return_data) == 32
-            assert int.from_bytes(return_data, "big") == 1
-            assert gas_used == 3450
-
-        @pytest.mark.parametrize(
-            "address, input_data",
-            [
+                    [0] * 31 + [1],
+                ),
                 (
                     0x100,
                     bytes.fromhex(
                         "5cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e"
                     ),
-                )
+                    [],
+                ),
             ],
         )
-        def test__p256_verify_precompile_should_fail(
-            self, cairo_run, address, input_data
+        def test__p256_verify_precompile(
+            self, cairo_run, address, input_data, expected_return_data
         ):
             return_data, reverted, gas_used = cairo_run(
                 "test__precompiles_run", address=address, input=input_data
             )
             assert not reverted
-            assert len(return_data) == 0
+            assert return_data == expected_return_data
             assert gas_used == 3450
 
     class TestIsPrecompile:
