3162e34501e08d3b8fdde205b0ca688a9bf90413
Clément Walter <clement0walter@gmail.com>
2024-06-28 11:22:02 +0200

    Fix bytes_to_bytes8_little test

--- tests/src/utils/test_bytes.py
+++ tests/src/utils/test_bytes.py
@@ -1,3 +1,6 @@
+import os
+import random
+
 import pytest
 
 from tests.utils.uint256 import int_to_uint256
@@ -60,62 +63,24 @@ class TestBytes:
 
     class TestBytesToBytes8LittleEndian:
 
-        @pytest.mark.parametrize("bytes_len", list(range(20)))
+        @pytest.mark.parametrize("bytes_len", [0, 10, 100, 1000, 10_000, 100_000])
         def test_should_return_bytes8(self, cairo_run, bytes_len):
-            bytes_array = list(range(bytes_len))
+            random.seed(bytes_len)
+            bytes_array = list(os.urandom(bytes_len))
             bytes8_little_endian = [
-                int.from_bytes(bytes(bytes_array[i : i + 8]), byteorder="little")
+                int.from_bytes(bytes(bytes_array[i : i + 8]), "little")
                 for i in range(0, len(bytes_array), 8)
             ]
-            # Case where the last word is not full
-            if bytes8_little_endian and not len(bytes_array) % 8 == 0:
-                last_expected_word = bytes8_little_endian[-1]
-                last_expected_word_bytes_used = (
-                    last_expected_word.bit_length() // 8 + 1
-                    if last_expected_word
-                    else 1
-                )
-            # Case where the last word is full - or no words at all
-            else:
-                last_expected_word = 0
-                last_expected_word_bytes_used = 0
+            output, last_word, last_word_len = cairo_run(
+                "test__bytes_to_bytes8_little_endian", bytes=bytes_array
+            )
+            last_word = int.to_bytes(
+                last_word, byteorder="little", length=last_word_len
+            )
+            output = (
+                output + [int.from_bytes(last_word, "little")]
+                if len(last_word) > 0
+                else output
+            )
 
-            output = cairo_run("test__bytes_to_bytes8_little_endian", bytes=bytes_array)
-
-            *full_words, last_word, last_word_num_bytes = output
-
-            if len(bytes_array) % 8 == 0:
-                assert bytes8_little_endian == full_words
-            else:
-                assert bytes8_little_endian[:-1] == full_words
-            assert last_expected_word == last_word
-            assert last_expected_word_bytes_used == last_word_num_bytes
-
-        @pytest.mark.parametrize(
-            "byte_values, expected_words, last_word, last_word_bytes_used",
-            [
-                ([], [], 0, 0),
-                ([0], [], 0, 1),
-                ([1, 2, 3, 4, 5, 6, 7, 8], [0x0807060504030201], 0, 0),
-                ([1, 2, 3, 4, 5, 6, 7, 8, 9], [0x0807060504030201], 9, 1),
-            ],
-        )
-        def test_should_return_bytes8_specific_inputs(
-            self,
-            cairo_run,
-            byte_values,
-            expected_words,
-            last_word,
-            last_word_bytes_used,
-        ):
-            output = cairo_run("test__bytes_to_bytes8_little_endian", bytes=byte_values)
-            *full_words, last_word_result, last_word_num_bytes = output
-            assert (
-                expected_words == full_words
-            ), f"Expected full words: {expected_words}, but got: {full_words}"
-            assert (
-                last_word == last_word_result
-            ), f"Expected last word: {last_word}, but got: {last_word_result}"
-            assert (
-                last_word_bytes_used == last_word_num_bytes
-            ), f"Expected last word bytes used: {last_word_bytes_used}, but got: {last_word_num_bytes}"
+            assert bytes8_little_endian == output
