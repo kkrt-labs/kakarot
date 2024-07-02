fc867c0c944f1a6876078e6122b784420e965681
Mathieu <60658558+enitrat@users.noreply.github.com>
2024-07-02 16:39:53 +0200

    chore: add solidity formatter (#1247)
    <!--- Please provide a general summary of your changes in the title
    above -->
    
    <!-- Give an estimate of the time you spent on this PR in terms of work
    days.
    Did you spend 0.5 days on this PR or rather 2 days?  -->
    
    Time spent on this PR: 0.5h
    
    ## Pull request type
    
    <!-- Please try to limit your pull request to one type,
    submit multiple pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## What is the current behavior?
    
    <!-- Please describe the current behavior that you are modifying,
    or link to a relevant issue. -->
    
    Resolves #1244
    
    ## What is the new behavior?
    
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    -
    -
    -
    
    <!-- Reviewable:start -->
    - - -
    This change is [<img src="https://reviewable.io/review_button.svg"
    height="34" align="absmiddle"
    alt="Reviewable"/>](https://reviewable.io/reviews/kkrt-labs/kakarot/1247)
    <!-- Reviewable:end -->

--- solidity_contracts/src/RIP7212/RIP7212Invoker.sol
+++ solidity_contracts/src/RIP7212/RIP7212Invoker.sol
@@ -1,11 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-contract RIP7212Invoker  {
-
+contract RIP7212Invoker {
     address constant P256_VERIFY = 0x0000000000000000000000000000000000000100;
 
-    function p256verify (bytes32 msg_hash, bytes32 r, bytes32 s, bytes32 x, bytes32 y) public view returns (bool) {
+    function p256verify(bytes32 msg_hash, bytes32 r, bytes32 s, bytes32 x, bytes32 y) public view returns (bool) {
         assembly {
             let pointer := mload(0x40)
 
@@ -17,9 +16,7 @@ contract RIP7212Invoker  {
             mstore(add(pointer, 0x80), y)
 
             // Staticcall into the precompile, revert if it fails
-            if iszero(staticcall(not(0), P256_VERIFY, pointer, 0xA0, pointer, 0x20)) {
-                revert(0, 0)
-            }
+            if iszero(staticcall(not(0), P256_VERIFY, pointer, 0xA0, pointer, 0x20)) { revert(0, 0) }
 
             // If the precompile returns no data, return false
             let size := returndatasize()
