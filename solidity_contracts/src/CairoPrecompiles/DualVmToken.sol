5c5aa1bb123b54802010d1570eace0c5c9aa7a2e
enitrat <msaug@protonmail.com>
2024-07-16 10:42:24 +0200

    fix casing in lib import

--- solidity_contracts/src/CairoPrecompiles/DualVmToken.sol
+++ solidity_contracts/src/CairoPrecompiles/DualVmToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity >=0.8.0;
 
-import {CairoLib} from "kakarot-lib/Cairolib.sol";
+import {CairoLib} from "kakarot-lib/CairoLib.sol";
 
 using CairoLib for uint256;
 
