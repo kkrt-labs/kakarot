ab9be1a00895bdf62d8d781ec9e37f142a81641f
Oba <obatirou@gmail.com>
2024-08-07 16:18:16 +0200

    Apply suggestions from code review
    Co-authored-by: Mathieu <60658558+enitrat@users.noreply.github.com>

--- tests/src/kakarot/test_evm.py
+++ tests/src/kakarot/test_evm.py
@@ -86,13 +86,13 @@ class TestIsValidJumpdest:
     # 1000000 is the default value for the init_evm test helper
     @given(amount=integers(min_value=0, max_value=1000000))
     def test_should_return_gas_left(self, cairo_run, amount):
-        output = cairo_run("test__charge_gas", amount=amount)
-        assert output[0] == 1000000 - amount  # gas_left
-        assert output[1] == 0  # stopped
+        gas_left, stopped = cairo_run("test__charge_gas", amount=amount)
+        assert gas_left == 1000000 - amount
+        assert stopped == 0
 
     # 1000000 is the default value for the init_evm test helper
     @given(amount=integers(min_value=1000001, max_value=2**248 - 1))
     def test_should_return_not_enough_gas(self, cairo_run, amount):
-        output = cairo_run("test__charge_gas", amount=amount)
-        assert output[0] == 0  # gas_left
-        assert output[1] == 1  # stopped
+        gas_left, stopped = cairo_run("test__charge_gas", amount=amount)
+        assert gas_left == 0
+        assert stopped == 1
