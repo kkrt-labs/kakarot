ce6aa444713d2862988d847a3555ec11e2c67b5f
Eugenio Paluello <eugypalu@gmail.com>
2024-09-13 09:49:51 +0200

    feat: get contracts from sepolia

--- kakarot_scripts/withdraw_accounts.py
+++ kakarot_scripts/withdraw_accounts.py
@@ -1,6 +1,9 @@
 # %% Imports
 import logging
+import os
 from asyncio import run
+
+import requests
 
 from kakarot_scripts.constants import ETH_TOKEN_ADDRESS, NETWORK, RPC_CLIENT
 from kakarot_scripts.utils.starknet import get_balance, get_declarations, invoke
@@ -10,12 +13,30 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
+# %% Fetch contract events
+def get_contracts():
+    url = f"https://sepolia-api.voyager.online/beta/events?ps=10&p=1&contract={os.getenv('KAKAROT_SEPOLIA_ACCOUNT_ADDRESS')}"
+    headers = {
+        "accept": "application/json",
+        "x-api-key": os.getenv("VOYAGER_API_KEY"),
+    }
+    response = requests.get(url, headers=headers)
+    return [
+        {
+            "evm_address": "0x" + raw[2:22].hex(),
+            "starknet_address": "0x" + raw[23:].hex(),
+        }
+        for raw in (
+            bytes(contract["data"]["data"]) for contract in response.json()["items"]
+        )
+    ]
+
+
 # %% Main
 async def main():
     # %% Withdraw all accounts
 
-    # TODO: List of {evm_address, starknet_address} dict to be retrieved from parsing Kakarot contract events
-    contracts = []
+    contracts = get_contracts()
 
     balance_prev = await get_balance(NETWORK["account_address"])
     logger.info(f"ℹ️  Current deployer balance {balance_prev / 1e18} ETH")
