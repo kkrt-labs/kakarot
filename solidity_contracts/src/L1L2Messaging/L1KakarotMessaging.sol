4b2a0d0c352c0eff90cb08d4481971da5ace0112
Oba <obatirou@gmail.com>
2024-09-16 17:30:32 +0200

    test: L1KakarotMessaging

--- solidity_contracts/src/L1L2Messaging/L1KakarotMessaging.sol
+++ solidity_contracts/src/L1L2Messaging/L1KakarotMessaging.sol
@@ -9,12 +9,13 @@ interface IL1KakarotMessaging {
 }
 
 contract L1KakarotMessaging {
-    uint256 constant HANDLE_L1_MESSAGE_SELECTOR = uint256(keccak256("handle_l1_message")) % 2 ** 250;
-    IStarknetMessaging private immutable _starknetMessaging;
+    uint256 public constant HANDLE_L1_MESSAGE_SELECTOR = uint256(keccak256("handle_l1_message")) % 2 ** 250;
+
+    IStarknetMessaging public immutable starknetMessaging;
     uint256 public immutable kakarotAddress;
 
     constructor(address starknetMessaging_, uint256 kakarotAddress_) {
-        _starknetMessaging = IStarknetMessaging(starknetMessaging_);
+        starknetMessaging = IStarknetMessaging(starknetMessaging_);
         kakarotAddress = kakarotAddress_;
     }
 
@@ -25,17 +26,18 @@ contract L1KakarotMessaging {
     /// @param value The value to send to the contract on L2. The value is taken from the L2 contract address.
     /// @param data The data to send to the contract on L2.
     function sendMessageToL2(address to, uint248 value, bytes calldata data) external payable {
-        uint256[] memory convertedData = new uint256[](data.length + 4);
+        uint256 totalLength = data.length + 4;
+        uint256[] memory convertedData = new uint256[](totalLength);
         convertedData[0] = uint256(uint160(msg.sender));
         convertedData[1] = uint256(uint160(to));
         convertedData[2] = uint256(value);
-        convertedData[3] = uint256(data.length);
-        for (uint256 i = 4; i < data.length + 4; i++) {
+        convertedData[3] = data.length;
+        for (uint256 i = 4; i < totalLength; ++i) {
             convertedData[i] = uint256(uint8(data[i - 4]));
         }
 
         // Send the converted data to L2
-        _starknetMessaging.sendMessageToL2{value: msg.value}(kakarotAddress, HANDLE_L1_MESSAGE_SELECTOR, convertedData);
+        starknetMessaging.sendMessageToL2{value: msg.value}(kakarotAddress, HANDLE_L1_MESSAGE_SELECTOR, convertedData);
     }
 
     /// @notice Consumes a message sent from L2.
@@ -45,10 +47,11 @@ contract L1KakarotMessaging {
         // Will revert if the message is not consumable.
         // Consider each byte of calldata as a uint256.
         bytes memory fullPayload = abi.encode(msg.sender, fromAddress, payload);
-        uint256[] memory convertedPayload = new uint256[](fullPayload.length);
-        for (uint256 i = 0; i < fullPayload.length; i++) {
+        uint256 totalLength = fullPayload.length;
+        uint256[] memory convertedPayload = new uint256[](totalLength);
+        for (uint256 i = 0; i < totalLength; ++i) {
             convertedPayload[i] = uint256(uint8(fullPayload[i]));
         }
-        _starknetMessaging.consumeMessageFromL2(kakarotAddress, convertedPayload);
+        starknetMessaging.consumeMessageFromL2(kakarotAddress, convertedPayload);
     }
 }
