e2cefdaa7fbc349a6bff56a6e2e616172e05961d
Oba <obatirou@gmail.com>
2024-09-18 10:34:58 +0200

    test: L1KakarotMessaging (#1416)
    <!--- Please provide a general summary of your changes in the title
    above -->
    
    <!-- Give an estimate of the time you spent on this PR in terms of work
    days.
    Did you spend 0.5 days on this PR or rather 2 days?  -->
    
    Time spent on this PR:
    
    ## Pull request type
    
    <!-- Please try to limit your pull request to one type,
    submit multiple pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [x] Other (please describe): testing and opti
    
    ## What is the current behavior?
    
    <!-- Please describe the current behavior that you are modifying,
    or link to a relevant issue. -->
    
    Resolves #<Issue number>
    
    ## What is the new behavior?
    
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    - add foundry unit test for L1KakarotMessaging
    
    <!-- Reviewable:start -->
    - - -
    This change is [<img src="https://reviewable.io/review_button.svg"
    height="34" align="absmiddle"
    alt="Reviewable"/>](https://reviewable.io/reviews/kkrt-labs/kakarot/1416)
    <!-- Reviewable:end -->

---
+++ solidity_contracts/tests/L1KakarotMessage.t.sol
@@ -0,0 +1,69 @@
+pragma solidity >=0.8.0;
+
+import "forge-std/Test.sol";
+
+import {L1KakarotMessaging} from "../src/L1L2Messaging/L1KakarotMessaging.sol";
+import {StarknetMessagingLocal} from "../src/starknet/StarknetMessagingLocal.sol";
+
+contract L1KakarotMessagingTest is Test {
+    L1KakarotMessaging l1KakarotMessaging;
+    StarknetMessagingLocal starknetMessagingLocal;
+    uint256 mockedKakarot = 0xFF1;
+
+    function setUp() public {
+        starknetMessagingLocal = new StarknetMessagingLocal();
+        l1KakarotMessaging = new L1KakarotMessaging(address(starknetMessagingLocal), mockedKakarot);
+    }
+
+    function getL1ToL2MsgHash(uint256 toAddress, uint256 selector, uint256[] memory payload, uint256 nonce)
+        internal
+        view
+        returns (bytes32)
+    {
+        return keccak256(
+            abi.encodePacked(
+                uint256(uint160(address(l1KakarotMessaging))), toAddress, nonce, selector, payload.length, payload
+            )
+        );
+    }
+
+    function test_sendMessageToL2(address to, uint248 value, bytes memory data) public {
+        vm.assume(value <= starknetMessagingLocal.getMaxL1MsgFee());
+        deal(address(this), 100 ether);
+        l1KakarotMessaging.sendMessageToL2{value: 0.1 ether}(to, value, data);
+
+        uint256 totalLength = data.length + 4;
+        uint256[] memory convertedData = new uint256[](totalLength);
+        convertedData[0] = uint256(uint160(address(this)));
+        convertedData[1] = uint256(uint160(to));
+        convertedData[2] = value;
+        convertedData[3] = data.length;
+        for (uint256 i = 4; i < totalLength; ++i) {
+            convertedData[i] = uint256(uint8(data[i - 4]));
+        }
+
+        bytes32 msgHash =
+            getL1ToL2MsgHash(mockedKakarot, l1KakarotMessaging.HANDLE_L1_MESSAGE_SELECTOR(), convertedData, 0);
+
+        assertEq(starknetMessagingLocal.l1ToL2Messages(msgHash), 0.1 ether + 1);
+    }
+
+    function test_consumeMessageFromL2(address fromAddress, bytes memory payload) public {
+        deal(address(this), 100 ether);
+        bytes memory fullPayload = abi.encode(address(this), fromAddress, payload);
+        uint256 totalLength = fullPayload.length;
+        uint256[] memory convertedPayload = new uint256[](totalLength);
+        for (uint256 i = 0; i < totalLength; ++i) {
+            convertedPayload[i] = uint256(uint8(fullPayload[i]));
+        }
+        // Ensures the consumeMessageFromL2 function is called with the correct parameters.
+        vm.mockCall(
+            address(starknetMessagingLocal),
+            abi.encodeWithSelector(
+                starknetMessagingLocal.consumeMessageFromL2.selector, mockedKakarot, convertedPayload
+            ),
+            abi.encode(true)
+        );
+        l1KakarotMessaging.consumeMessageFromL2(fromAddress, payload);
+    }
+}
