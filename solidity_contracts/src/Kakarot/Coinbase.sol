33b7710d458db2632193346ca66df9c274bffffb
Clément Walter <clement0walter@gmail.com>
2024-10-09 18:34:37 +0200

    Fix PR comment apart from Ownable2Step

--- solidity_contracts/src/Kakarot/Coinbase.sol
+++ solidity_contracts/src/Kakarot/Coinbase.sol
@@ -4,12 +4,11 @@ pragma solidity >=0.8.0 <0.9.0;
 import {DualVmToken} from "../CairoPrecompiles/DualVmToken.sol";
 
 contract Coinbase {
+    /// @dev The EVM address of the DualVmToken for Kakarot ETH.
+    DualVmToken public immutable kakarotEth;
+
     /// @dev State variable to store the owner of the contract
     address public owner;
-
-    /// @dev The EVM address of the DualVmToken for Kakarot ETH.
-    /// @dev DualVmToken.balanceOf(this) is the same as address(this).balance
-    DualVmToken immutable kakarotEth;
 
     /// Constructor sets the owner of the contract
     constructor(address _kakarotEth) {
@@ -18,12 +17,15 @@ contract Coinbase {
     }
 
     /// Modifier to restrict access to owner only
+    /// @dev Assert that msd.sender is the owner
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the contract owner");
         _;
     }
 
     /// @notice Withdraws ETH from the contract to a Starknet address
+    /// @dev DualVmToken.balanceOf(this) is the same as address(this).balance
+    /// @param toStarknetAddress The Starknet address to withdraw to
     function withdraw(uint256 toStarknetAddress) external onlyOwner {
         uint256 balance = address(this).balance;
         kakarotEth.transfer(toStarknetAddress, balance);
