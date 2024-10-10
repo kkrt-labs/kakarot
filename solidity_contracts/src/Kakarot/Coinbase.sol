3cf01745b4c415cb3f42dcb9940a2efe90320261
Oba <obatirou@gmail.com>
2024-10-10 17:31:02 +0200

    feat: Ownable2Step coinbase

--- solidity_contracts/src/Kakarot/Coinbase.sol
+++ solidity_contracts/src/Kakarot/Coinbase.sol
@@ -2,25 +2,15 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import {DualVmToken} from "../CairoPrecompiles/DualVmToken.sol";
+import {Ownable2Step, Ownable} from "@openzeppelin-contracts/access/Ownable2Step.sol";
 
-contract Coinbase {
+contract Coinbase is Ownable2Step {
     /// @dev The EVM address of the DualVmToken for Kakarot ETH.
     DualVmToken public immutable kakarotEth;
 
-    /// @dev State variable to store the owner of the contract
-    address public owner;
-
     /// Constructor sets the owner of the contract
-    constructor(address _kakarotEth) {
-        owner = msg.sender;
+    constructor(address _kakarotEth) Ownable(msg.sender) {
         kakarotEth = DualVmToken(_kakarotEth);
-    }
-
-    /// Modifier to restrict access to owner only
-    /// @dev Assert that msd.sender is the owner
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not the contract owner");
-        _;
     }
 
     /// @notice Withdraws ETH from the contract to a Starknet address
@@ -29,12 +19,5 @@ contract Coinbase {
     function withdraw(uint256 toStarknetAddress) external onlyOwner {
         uint256 balance = address(this).balance;
         kakarotEth.transfer(toStarknetAddress, balance);
-    }
-
-    /// @notice Transfers ownership of the contract to a new address
-    /// @param newOwner The address to transfer ownership to
-    function transferOwnership(address newOwner) external onlyOwner {
-        require(newOwner != address(0), "New owner cannot be the zero address");
-        owner = newOwner;
     }
 }
