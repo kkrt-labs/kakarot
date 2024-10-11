6fac1fc5deb0803a0fbe34950ddf2ddfd204f7b1
Oba <obatirou@gmail.com>
2024-10-11 11:45:15 +0200

    tests: add hypothesis profile to root conftest

--- cairo_zero/tests/conftest.py
+++ cairo_zero/tests/conftest.py
@@ -1,36 +1,4 @@
 import logging
-import os
 
-from dotenv import load_dotenv
-from hypothesis import Phase, Verbosity, settings
-
-load_dotenv()
 logging.getLogger("asyncio").setLevel(logging.ERROR)
 logger = logging.getLogger()
-
-settings.register_profile(
-    "nightly",
-    deadline=None,
-    max_examples=1500,
-    phases=[Phase.explicit, Phase.reuse, Phase.generate, Phase.target],
-)
-settings.register_profile(
-    "ci",
-    deadline=None,
-    max_examples=100,
-    phases=[Phase.explicit, Phase.reuse, Phase.generate, Phase.target],
-)
-settings.register_profile(
-    "dev",
-    deadline=None,
-    max_examples=10,
-    phases=[Phase.explicit, Phase.reuse, Phase.generate, Phase.target],
-)
-settings.register_profile(
-    "debug",
-    max_examples=10,
-    verbosity=Verbosity.verbose,
-    phases=[Phase.explicit, Phase.reuse, Phase.generate, Phase.target],
-)
-settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
-logger.info(f"Using Hypothesis profile: {os.getenv('HYPOTHESIS_PROFILE', 'default')}")
