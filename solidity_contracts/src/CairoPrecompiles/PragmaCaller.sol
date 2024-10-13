802281185ea9e73e01865371d32e64db8a883148
akhercha <adel@pragma.build>
2024-10-13 17:13:44 +0200

    feat(pragma_caller): Updated the Pragma Caller contract

--- solidity_contracts/src/CairoPrecompiles/PragmaCaller.sol
+++ solidity_contracts/src/CairoPrecompiles/PragmaCaller.sol
@@ -9,6 +9,45 @@ contract PragmaCaller {
     /// @dev The starknet address of the pragma oracle
     uint256 pragmaOracle;
 
+    /// @dev The starknet address of the pragma summary stats
+    uint256 pragmaSummaryStats;
+
+    /// @dev The cairo function selector to call `get_data` from the Pragma Oracle
+    uint256 constant FUNCTION_SELECTOR_GET_DATA =
+        uint256(keccak256("get_data")) % 2 ** 250;
+
+    /// @dev The cairo function selector to call `calculate_mean` from the Pragma Summary Stats
+    uint256 constant FUNCTION_SELECTOR_CALCULATE_MEAN =
+        uint256(keccak256("calculate_mean")) % 2 ** 250;
+
+    /// @dev The cairo function selector to call `calculate_volatility` from the Pragma Summary Stats
+    uint256 constant FUNCTION_SELECTOR_CALCULATE_VOLATILITY =
+        uint256(keccak256("calculate_volatility")) % 2 ** 250;
+
+    /// @dev The cairo function selector to call `calculate_twap` from the Pragma Summary Stats
+    uint256 constant FUNCTION_SELECTOR_CALCULATE_TWAP =
+        uint256(keccak256("calculate_twap")) % 2 ** 250;
+
+    /// @dev The aggregation mode used by the Oracle
+    enum AggregationMode {
+        Median,
+        Mean
+    }
+
+    /// @dev The request data type
+    enum DataType {
+        SpotEntry,
+        FuturesEntry,
+        GenericEntry
+    }
+
+    struct PragmaPricesRequest {
+        AggregationMode aggregationMode;
+        DataType dataType;
+        uint256 pairId;
+        uint256 expirationTimestamp;
+    }
+
     struct PragmaPricesResponse {
         uint256 price;
         uint256 decimals;
@@ -17,33 +56,67 @@ contract PragmaCaller {
         uint256 maybe_expiration_timestamp;
     }
 
-    enum DataType {
-        SpotEntry,
-        FuturesEntry,
-        GenericEntry
-    }
-
-    struct DataRequest {
+    struct PragmaCalculateMeanRequest {
+        AggregationMode aggregationMode;
         DataType dataType;
         uint256 pairId;
         uint256 expirationTimestamp;
+        uint64 startTimestamp;
+        uint64 endTimestamp;
     }
 
-    constructor(uint256 pragmaOracleAddress) {
+    struct PragmaCalculateVolatilityRequest {
+        AggregationMode aggregationMode;
+        DataType dataType;
+        uint256 pairId;
+        uint256 expirationTimestamp;
+        uint64 startTimestamp;
+        uint64 endTimestamp;
+        uint64 numSamples;
+    }
+
+    struct PragmaCalculateTwapRequest {
+        AggregationMode aggregationMode;
+        DataType dataType;
+        uint256 pairId;
+        uint256 expirationTimestamp;
+        uint64 startTimestamp;
+        uint64 durationInSeconds;
+    }
+
+    struct PragmaSummaryStatsResponse {
+        uint256 price;
+        uint256 decimals;
+    }
+
+    constructor(
+        uint256 pragmaOracleAddress,
+        uint256 pragmaSummaryStatsAddress
+    ) {
         pragmaOracle = pragmaOracleAddress;
+        pragmaSummaryStats = pragmaSummaryStatsAddress;
     }
 
-    function getDataMedianSpot(DataRequest memory request) public view returns (PragmaPricesResponse memory response) {
-        // Serialize the data request into a format compatible with the expected Pragma inputs - [enumIndex, [variantValues...]
-        // expirationTimestamp is only used for FuturesEntry requests - skip it for SpotEntry requests and GenericEntry requests
-        uint256[] memory data = new uint256[](request.dataType == DataType.FuturesEntry ? 3 : 2);
+    function getPrice(
+        PragmaPricesRequest memory request
+    ) public view returns (PragmaPricesResponse memory response) {
+        // Serialize the data request into a format compatible with the expected Pragma inputs
+        uint256[] memory data = new uint256[](
+            request.dataType == DataType.FuturesEntry ? 4 : 3
+        );
         data[0] = uint256(request.dataType);
         data[1] = request.pairId;
         if (request.dataType == DataType.FuturesEntry) {
             data[2] = request.expirationTimestamp;
+            data[3] = uint256(request.aggregationMode);
+        } else {
+            data[2] = uint256(request.aggregationMode);
         }
 
-        bytes memory returnData = pragmaOracle.staticcallCairo("get_data_median", data);
+        bytes memory returnData = pragmaOracle.staticcallCairo(
+            FUNCTION_SELECTOR_GET_DATA,
+            data
+        );
 
         assembly {
             // Load the values from the return data
@@ -57,7 +130,9 @@ contract PragmaCaller {
             // and we set it to 0 - otherwise, we load it from the return data
             let never_expires := mload(add(returnData, 0xa0))
             let maybe_expiration_timestamp := 0
-            if eq(never_expires, 0) { maybe_expiration_timestamp := mload(add(returnData, 0xc0)) }
+            if eq(never_expires, 0) {
+                maybe_expiration_timestamp := mload(add(returnData, 0xc0))
+            }
 
             // Store the values in the response struct
             mstore(response, price)
@@ -65,6 +140,122 @@ contract PragmaCaller {
             mstore(add(response, 0x40), last_updated_timestamp)
             mstore(add(response, 0x60), num_sources_aggregated)
             mstore(add(response, 0x80), maybe_expiration_timestamp)
+        }
+        return response;
+    }
+
+    function calculateMean(
+        PragmaCalculateMeanRequest memory request
+    ) public view returns (PragmaSummaryStatsResponse memory response) {
+        // Serialize the data request into a format compatible with the expected Pragma inputs
+        uint256[] memory data = new uint256[](
+            request.dataType == DataType.FuturesEntry ? 6 : 5
+        );
+        data[0] = uint256(request.dataType);
+        data[1] = request.pairId;
+        if (request.dataType == DataType.FuturesEntry) {
+            data[2] = request.expirationTimestamp;
+            data[3] = request.startTimestamp;
+            data[4] = request.endTimestamp;
+            data[5] = uint256(request.aggregationMode);
+        } else {
+            data[2] = uint256(request.startTimestamp);
+            data[3] = uint256(request.endTimestamp);
+            data[4] = uint256(request.aggregationMode);
+        }
+
+        bytes memory returnData = pragmaOracle.staticcallCairo(
+            FUNCTION_SELECTOR_CALCULATE_MEAN,
+            data
+        );
+
+        assembly {
+            // Load the values from the return data
+            // returnData[0:32] is the length of the return data
+            let price := mload(add(returnData, 0x20))
+            let decimals := mload(add(returnData, 0x40))
+
+            // Store the values in the response struct
+            mstore(response, price)
+            mstore(add(response, 0x20), decimals)
+        }
+        return response;
+    }
+
+    function calculateVolatility(
+        PragmaCalculateVolatilityRequest memory request
+    ) public view returns (PragmaSummaryStatsResponse memory response) {
+        // Serialize the data request into a format compatible with the expected Pragma inputs
+        uint256[] memory data = new uint256[](
+            request.dataType == DataType.FuturesEntry ? 7 : 6
+        );
+        data[0] = uint256(request.dataType);
+        data[1] = request.pairId;
+        if (request.dataType == DataType.FuturesEntry) {
+            data[2] = request.expirationTimestamp;
+            data[3] = request.startTimestamp;
+            data[4] = request.endTimestamp;
+            data[5] = request.numSamples;
+            data[6] = uint256(request.aggregationMode);
+        } else {
+            data[2] = request.startTimestamp;
+            data[3] = request.endTimestamp;
+            data[4] = request.numSamples;
+            data[5] = uint256(request.aggregationMode);
+        }
+
+        bytes memory returnData = pragmaOracle.staticcallCairo(
+            FUNCTION_SELECTOR_CALCULATE_VOLATILITY,
+            data
+        );
+
+        assembly {
+            // Load the values from the return data
+            // returnData[0:32] is the length of the return data
+            let price := mload(add(returnData, 0x20))
+            let decimals := mload(add(returnData, 0x40))
+
+            // Store the values in the response struct
+            mstore(response, price)
+            mstore(add(response, 0x20), decimals)
+        }
+        return response;
+    }
+
+    function calculateTwap(
+        PragmaCalculateTwapRequest memory request
+    ) public view returns (PragmaSummaryStatsResponse memory response) {
+        // Serialize the data request into a format compatible with the expected Pragma inputs
+        uint256[] memory data = new uint256[](
+            request.dataType == DataType.FuturesEntry ? 6 : 5
+        );
+        data[0] = uint256(request.dataType);
+        data[1] = request.pairId;
+        if (request.dataType == DataType.FuturesEntry) {
+            data[2] = request.expirationTimestamp;
+            data[3] = uint256(request.aggregationMode);
+            data[4] = request.startTimestamp;
+            data[5] = request.durationInSeconds;
+        } else {
+            data[2] = uint256(request.aggregationMode);
+            data[3] = request.startTimestamp;
+            data[4] = request.durationInSeconds;
+        }
+
+        bytes memory returnData = pragmaOracle.staticcallCairo(
+            FUNCTION_SELECTOR_CALCULATE_TWAP,
+            data
+        );
+
+        assembly {
+            // Load the values from the return data
+            // returnData[0:32] is the length of the return data
+            let price := mload(add(returnData, 0x20))
+            let decimals := mload(add(returnData, 0x40))
+
+            // Store the values in the response struct
+            mstore(response, price)
+            mstore(add(response, 0x20), decimals)
         }
         return response;
     }
