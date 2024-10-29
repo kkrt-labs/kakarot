f3e57ced1d00e52ebe1572f3589e798befaa0a55
akhercha <akherchache@pm.me>
2024-10-29 15:21:39 +0100

    feat(pragma_caller): Lint 💀

--- tests/end_to_end/CairoPrecompiles/Pragma/test_pragma_precompile.py
+++ tests/end_to_end/CairoPrecompiles/Pragma/test_pragma_precompile.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import Optional, OrderedDict, Tuple, List
+from typing import List, Optional, OrderedDict, Tuple
 
 import pytest
 import pytest_asyncio
