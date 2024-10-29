efb19d35e86508f840eeef5dce648dcfeda59897
akhercha <akherchache@pm.me>
2024-10-29 10:51:26 +0100

    feat(pragma_caller): Updating tests

--- tests/end_to_end/CairoPrecompiles/Pragma/test_pragma_precompile.py
+++ tests/end_to_end/CairoPrecompiles/Pragma/test_pragma_precompile.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum
 from typing import OrderedDict, Tuple
 
@@ -6,6 +7,29 @@ import pytest_asyncio
 
 from kakarot_scripts.utils.kakarot import deploy
 from kakarot_scripts.utils.starknet import get_contract, get_deployments, invoke
+
+
+@dataclass
+class Entry:
+    key: int
+    expiration_timestamp: Optional[int]
+    is_generic: bool = False
+
+    @property
+    def entry_type(self) -> int:
+        if self.is_generic:
+            return 2
+        return 0 if self.expiration_timestamp is None else 1
+
+    def to_dict(self) -> dict:
+        if self.expiration_timestamp is None:
+            return {"SpotEntry": self.key}
+        if self.is_generic:
+            return {"GenericEntry": self.key}
+        return {"FutureEntry": (self.key, self.expiration_timestamp)}
+
+    def serialize(self) -> Tuple[int, int, int]:
+        return (self.entry_type, self.key, self.expiration_timestamp or 0)
 
 
 class AggregationMode(Enum):
@@ -19,9 +43,6 @@ class AggregationMode(Enum):
         return list(AggregationMode).index(self)
 
 
-ENTRY_TYPE_INDEX = {"SpotEntry": 0, "FutureEntry": 1, "GenericEntry": 2}
-
-
 def serialize_cairo_response(cairo_dict: OrderedDict) -> Tuple:
     """
     Serialize the return data of a Cairo call to a tuple
@@ -31,32 +52,35 @@ def serialize_cairo_response(cairo_dict: OrderedDict) -> Tuple:
     return tuple(value if value is not None else 0 for value in cairo_dict.values())
 
 
-def serialize_cairo_inputs(data_type: dict, aggregation_mode: AggregationMode) -> Tuple:
+def serialize_cairo_inputs(*args) -> Tuple[int, ...]:
     """
-    Serialize the data type & aggregation_mode to a tuple
-    with the same format as the one expected by the Solidity contract.
+    Serialize arguments to the same format as the one expected by the Solidity
+    contract.
 
-    In solidity, the serialized data type is a tuple with the following format:
-    (entry_type, pair_id, expiration_timestamp, aggregation_mode)
-      - SpotEntry and GenericEntry take one argument pair_id
-      - FutureEntry takes two arguments pair_id and expiration_timestamp
+    Args:
+        *args: Variable number of arguments. Each argument can be:
+            - AggregationMode: will be serialized using .serialize()
+            - Entry: will be serialized using .to_tuple()
+            - int: will be used as-is
 
-    The `expiration_timestamp` is set to 0 for SpotEntry and GenericEntry.
+    Returns:
+        Tuple[int, ...]: A flattened tuple containing only integers
     """
-    entry_type, query_args = next(iter(data_type.items()))
-    serialized_entry_type = ENTRY_TYPE_INDEX[entry_type]
-    serialized_aggregation_mode = aggregation_mode.serialize()
-
-    if isinstance(query_args, tuple):
-        pair_id, expiration_timestamp = query_args
-        return (
-            serialized_aggregation_mode,
-            serialized_entry_type,
-            pair_id,
-            expiration_timestamp,
-        )
-    else:
-        return (serialized_aggregation_mode, serialized_entry_type, query_args, 0)
+    serialized_inputs = []
+    for arg in args:
+        if isinstance(arg, (AggregationMode, Entry)):
+            serialized = arg.serialize()
+            if isinstance(serialized, tuple):
+                serialized_inputs.extend(serialized)
+            else:
+                serialized_inputs.append(serialized)
+        elif isinstance(arg, int):
+            serialized_inputs.append(arg)
+        else:
+            raise TypeError(
+                f"Unsupported type: {type(arg)}. Must be AggregationMode, Entry, or int"
+            )
+    return tuple(serialized_inputs)
 
 
 @pytest_asyncio.fixture(scope="module")
@@ -73,7 +97,7 @@ async def pragma_caller(owner):
 
 
 @pytest_asyncio.fixture()
-async def cairo_pragma(mocked_values, pragma_caller):
+async def cairo_pragma_oracle(mocked_values, pragma_caller):
     await invoke("MockPragmaOracle", "set_price", *mocked_values)
     await invoke(
         "kakarot",
@@ -84,6 +108,11 @@ async def cairo_pragma(mocked_values, pragma_caller):
     return get_contract("MockPragmaOracle")
 
 
+@pytest_asyncio.fixture()
+async def cairo_pragma_summary_stats():
+    return get_contract("MockPragmaSummaryStats")
+
+
 @pytest.mark.asyncio(scope="module")
 @pytest.mark.CairoPrecompiles
 class TestPragmaPrecompile:
@@ -92,7 +121,7 @@ class TestPragmaPrecompile:
         "data_type, aggregation_mode, mocked_values",
         [
             (
-                {"SpotEntry": int.from_bytes(b"BTC/USD", byteorder="big")},
+                Entry(key=int.from_bytes(b"BTC/USD", byteorder="big")),
                 AggregationMode.MEDIAN,
                 (
                     int.from_bytes(b"BTC/USD", byteorder="big"),
@@ -103,7 +132,10 @@ class TestPragmaPrecompile:
                 ),
             ),
             (
-                {"FutureEntry": (int.from_bytes(b"ETH/USD", byteorder="big"), 0)},
+                Entry(
+                    key=int.from_bytes(b"ETH/USD", byteorder="big"),
+                    expiration_timestamp=0,
+                ),
                 AggregationMode.MEDIAN,
                 (
                     int.from_bytes(b"ETH/USD", byteorder="big"),
@@ -114,7 +146,7 @@ class TestPragmaPrecompile:
                 ),
             ),
             (
-                {"GenericEntry": int.from_bytes(b"SOL/USD", byteorder="big")},
+                Entry(key=int.from_bytes(b"SOL/USD", byteorder="big"), is_generic=True),
                 AggregationMode.MEDIAN,
                 (
                     int.from_bytes(b"SOL/USD", byteorder="big"),
@@ -128,18 +160,18 @@ class TestPragmaPrecompile:
     )
     async def test_should_return_data_median_for_query(
         self,
-        cairo_pragma,
+        cairo_pragma_oracle,
         pragma_caller,
         data_type,
         aggregation_mode,
         mocked_values,
         max_fee,
     ):
-        (cairo_res,) = await cairo_pragma.functions["get_data"].call(
-            data_type,
+        (cairo_res,) = await cairo_pragma_oracle.functions["get_data"].call(
+            data_type.to_dict(),
             aggregation_mode.to_tuple(),
         )
-        solidity_input = serialize_cairo_inputs(data_type, aggregation_mode)
+        solidity_input = serialize_cairo_inputs(aggregation_mode, data_type)
         sol_res = await pragma_caller.getData(solidity_input)
         serialized_cairo_res = serialize_cairo_response(cairo_res)
         assert serialized_cairo_res == sol_res
@@ -169,3 +201,198 @@ class TestPragmaPrecompile:
             if data_type.get("FutureEntry")
             else 0
         )
+
+    @pytest.mark.parametrize(
+        "data_type, aggregation_mode, mocked_values",
+        [
+            (
+                Entry(key=int.from_bytes(b"BTC/USD", byteorder="big")),
+                AggregationMode.MEDIAN,
+                (
+                    int.from_bytes(b"BTC/USD", byteorder="big"),
+                    70000,
+                    18,
+                    1717143838,
+                    1,
+                ),
+            ),
+            (
+                Entry(
+                    key=int.from_bytes(b"ETH/USD", byteorder="big"),
+                    expiration_timestamp=0,
+                ),
+                AggregationMode.MEDIAN,
+                (
+                    int.from_bytes(b"ETH/USD", byteorder="big"),
+                    4000,
+                    18,
+                    1717143838,
+                    1,
+                ),
+            ),
+        ],
+    )
+    async def test_should_get_mean_for_query(
+        self,
+        cairo_pragma_oracle,
+        cairo_pragma_summary_stats,
+        pragma_caller,
+        data_type,
+        aggregation_mode,
+        mocked_values,
+        max_fee,
+    ):
+        (cairo_res,) = await cairo_pragma_summary_stats.functions[
+            "calculate_mean"
+        ].call(
+            data_type.to_tuple(),
+            0,
+            0,
+            aggregation_mode.to_tuple(),
+        )
+        solidity_input = serialize_cairo_inputs(data_type, 0, 0, aggregation_mode)
+        sol_res = await pragma_caller.calculateMean(solidity_input)
+        serialized_cairo_res = serialize_cairo_response(cairo_res)
+        assert serialized_cairo_res == sol_res
+
+        (
+            res_price,
+            res_decimals,
+        ) = sol_res
+        (
+            _,
+            mocked_price,
+            mocked_decimals,
+        ) = mocked_values
+        assert res_price == mocked_price
+        assert res_decimals == mocked_decimals
+
+    @pytest.mark.parametrize(
+        "data_type, aggregation_mode, mocked_values",
+        [
+            (
+                Entry(key=int.from_bytes(b"BTC/USD", byteorder="big")),
+                AggregationMode.MEDIAN,
+                (
+                    int.from_bytes(b"BTC/USD", byteorder="big"),
+                    70000,
+                    18,
+                    1717143838,
+                    1,
+                ),
+            ),
+            (
+                Entry(
+                    key=int.from_bytes(b"ETH/USD", byteorder="big"),
+                    expiration_timestamp=0,
+                ),
+                AggregationMode.MEDIAN,
+                (
+                    int.from_bytes(b"ETH/USD", byteorder="big"),
+                    4000,
+                    18,
+                    1717143838,
+                    1,
+                ),
+            ),
+        ],
+    )
+    async def test_should_get_volatility_for_query(
+        self,
+        cairo_pragma_oracle,
+        cairo_pragma_summary_stats,
+        pragma_caller,
+        data_type,
+        aggregation_mode,
+        mocked_values,
+        max_fee,
+    ):
+        (cairo_res,) = await cairo_pragma_summary_stats.functions[
+            "calculate_volatility"
+        ].call(
+            data_type.to_tuple(),
+            0,
+            0,
+            0,
+            aggregation_mode.to_tuple(),
+        )
+        solidity_input = serialize_cairo_inputs(data_type, 0, 0, 0, aggregation_mode)
+        sol_res = await pragma_caller.calculateVolatility(solidity_input)
+        serialized_cairo_res = serialize_cairo_response(cairo_res)
+        assert serialized_cairo_res == sol_res
+
+        (
+            res_price,
+            res_decimals,
+        ) = sol_res
+        (
+            _,
+            mocked_price,
+            mocked_decimals,
+        ) = mocked_values
+        assert res_price == mocked_price
+        assert res_decimals == mocked_decimals
+
+    @pytest.mark.parametrize(
+        "data_type, aggregation_mode, mocked_values",
+        [
+            (
+                Entry(key=int.from_bytes(b"BTC/USD", byteorder="big")),
+                AggregationMode.MEDIAN,
+                (
+                    int.from_bytes(b"BTC/USD", byteorder="big"),
+                    70000,
+                    18,
+                    1717143838,
+                    1,
+                ),
+            ),
+            (
+                Entry(
+                    key=int.from_bytes(b"ETH/USD", byteorder="big"),
+                    expiration_timestamp=0,
+                ),
+                AggregationMode.MEDIAN,
+                (
+                    int.from_bytes(b"ETH/USD", byteorder="big"),
+                    4000,
+                    18,
+                    1717143838,
+                    1,
+                ),
+            ),
+        ],
+    )
+    async def test_should_get_twap_for_query(
+        self,
+        cairo_pragma_oracle,
+        cairo_pragma_summary_stats,
+        pragma_caller,
+        data_type,
+        aggregation_mode,
+        mocked_values,
+        max_fee,
+    ):
+        (cairo_res,) = await cairo_pragma_summary_stats.functions[
+            "calculate_twap"
+        ].call(
+            data_type.to_tuple(),
+            aggregation_mode.to_tuple(),
+            0,
+            0,
+        )
+        solidity_input = serialize_cairo_inputs(data_type, aggregation_mode, 0, 0)
+        sol_res = await pragma_caller.calculateTwap(solidity_input)
+        assert serialized_cairo_res == sol_res
+
+        (
+            res_price,
+            res_decimals,
+        ) = sol_res
+        (
+            _,
+            mocked_price,
+            mocked_decimals,
+        ) = mocked_values
+        assert res_price == mocked_price
+        assert res_decimals == mocked_decimals
