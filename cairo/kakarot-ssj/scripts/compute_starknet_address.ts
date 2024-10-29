1001d6c77b8dcd4e91c530a9e6cf508dc93d2700
futreall <86553580+futreall@users.noreply.github.com>
2024-10-29 11:10:44 +0200

    Update compute_starknet_address.ts

--- cairo/kakarot-ssj/scripts/compute_starknet_address.ts
+++ cairo/kakarot-ssj/scripts/compute_starknet_address.ts
@@ -8,8 +8,8 @@ inquirer
       name: "classHashInput",
       message: "Enter the class hash:",
       validate: (input) => {
-        if (input.trim() === "") {
-          return "Class hash is required.";
+        if (!/^0x[0-9a-fA-F]+$/.test(input.trim())) {
+          return "Please enter a valid hexadecimal class hash.";
         }
         return true;
       },
@@ -17,15 +17,27 @@ inquirer
     {
       type: "input",
       name: "saltInput",
-      message: "Enter the salt",
+      message: "Enter the salt:",
       default: "0x65766d5f61646472657373",
+      validate: (input) => {
+        if (!/^0x[0-9a-fA-F]+$/.test(input.trim())) {
+          return "Please enter a valid hexadecimal salt.";
+        }
+        return true;
+      },
     },
     {
       type: "input",
       name: "deployerInput",
-      message: "Enter the deployer address",
+      message: "Enter the deployer address:",
       default:
         "0x7753aaa1814b9f978fd93b66453ae87419b66d764fbf9313847edeb0283ef63",
+      validate: (input) => {
+        if (!/^0x[0-9a-fA-F]+$/.test(input.trim())) {
+          return "Please enter a valid hexadecimal deployer address.";
+        }
+        return true;
+      },
     },
   ])
   .then((answers) => {
@@ -35,7 +47,7 @@ inquirer
 
     const CONSTRUCTOR_CALLDATA = [deployerAddress, salt];
 
-    function compute_starknet_address() {
+    function computeStarknetAddress() {
       return hash.calculateContractAddressFromHash(
         salt,
         classHash,
@@ -44,5 +56,5 @@ inquirer
       );
     }
 
-    console.log("Pre-computed Starknet Address: " + compute_starknet_address());
+    console.log("Pre-computed Starknet Address:", computeStarknetAddress());
   });
