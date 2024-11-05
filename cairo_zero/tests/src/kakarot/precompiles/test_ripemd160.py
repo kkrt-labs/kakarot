cb3998f6fa428326b7005a7e454342b1c9ecaad9
Oba <obatirou@gmail.com>
2024-11-05 15:12:00 +0100

    fix: off by one error ripemd-160

--- cairo_zero/tests/src/kakarot/precompiles/test_ripemd160.py
+++ cairo_zero/tests/src/kakarot/precompiles/test_ripemd160.py
@@ -1,6 +1,6 @@
 import pytest
 from Crypto.Hash import RIPEMD160
-from hypothesis import given, settings
+from hypothesis import example, given
 from hypothesis.strategies import binary
 
 
@@ -8,7 +8,9 @@ from hypothesis.strategies import binary
 @pytest.mark.slow
 class TestRIPEMD160:
     @given(msg_bytes=binary(min_size=1, max_size=200))
-    @settings(max_examples=3)
+    @example(
+        msg_bytes=b"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmomnopnopq"
+    )  # https://github.com/code-423n4/2024-09-kakarot-findings/issues/50
     async def test_ripemd160_should_return_correct_hash(self, cairo_run, msg_bytes):
         precompile_hash = cairo_run("test__ripemd160", msg=list(msg_bytes))
 
