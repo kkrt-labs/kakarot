b8853b47c60db907287c321bc9b12a1f20dde86c
Oba <obatirou@gmail.com>
2024-11-06 15:26:51 +0100

    [KGA-14] [KGA-28] [KGA-74] [KGA-132] [KGA-92] fix: finalize dictionary in RIPEMD160 (#1577)
    <!--- Please provide a general summary of your changes in the title
    above -->
    
    <!-- Give an estimate of the time you spent on this PR in terms of work
    days.
    Did you spend 0.5 days on this PR or rather 2 days?  -->
    
    Time spent on this PR:
    
    ## Pull request type
    
    <!-- Please try to limit your pull request to one type,
    submit multiple pull requests if needed. -->
    
    Please check the type of change your PR introduces:
    
    - [X] Bugfix
    - [ ] Feature
    - [ ] Code style update (formatting, renaming)
    - [ ] Refactoring (no functional changes, no api changes)
    - [ ] Build related changes
    - [ ] Documentation content changes
    - [ ] Other (please describe):
    
    ## What is the current behavior?
    
    <!-- Please describe the current behavior that you are modifying,
    or link to a relevant issue. -->
    
    Resolves #1561
    
    ## What is the new behavior?
    
    <!-- Please describe the behavior or changes that are being added by
    this PR. -->
    
    Finalize the dict and reassign start after reseting the dict.
    
    <!-- Reviewable:start -->
    - - -
    This change is [<img src="https://reviewable.io/review_button.svg"
    height="34" align="absmiddle"
    alt="Reviewable"/>](https://reviewable.io/reviews/kkrt-labs/kakarot/1577)
    <!-- Reviewable:end -->

--- cairo_zero/tests/src/kakarot/precompiles/test_ripemd160.py
+++ cairo_zero/tests/src/kakarot/precompiles/test_ripemd160.py
@@ -3,6 +3,9 @@ from Crypto.Hash import RIPEMD160
 from hypothesis import example, given
 from hypothesis.strategies import binary
 
+from tests.utils.errors import cairo_error
+from tests.utils.hints import insert_hint
+
 
 @pytest.mark.asyncio
 @pytest.mark.slow
@@ -20,3 +23,22 @@ class TestRIPEMD160:
         expected_hash = ripemd160_crypto.hexdigest()
 
         assert expected_hash.rjust(64, "0") == bytes(precompile_hash).hex()
+
+    # see https://github.com/code-423n4/2024-09-kakarot-findings/issues/54
+    # see https://github.com/code-423n4/2024-09-kakarot-findings/issues/21
+    async def test_finalized_dict_ripemd160(self, cairo_program, cairo_run):
+        msg_bytes = bytes([0x00] * 57)
+        with (
+            insert_hint(
+                cairo_program,
+                "ripemd160.cairo:161",
+                "try:\n"
+                "    dict_tracker = __dict_manager.get_tracker(ids.dict_ptr)\n"
+                "    dict_tracker.data[ids.index_4] = 1\n"
+                "except Exception: pass\n",
+            ),
+            cairo_error(
+                message="An ASSERT_EQ instruction failed"
+            ),  # fails with an assertion error from default_dict_finalize_inner
+        ):
+            cairo_run("test__ripemd160", msg=list(msg_bytes))
