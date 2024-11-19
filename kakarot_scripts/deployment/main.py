4abbdef4fc2829a876c988c28277c32acaf016dd
Oba <obatirou@gmail.com>
2024-11-19 11:15:16 +0100

    update scripts

--- kakarot_scripts/deployment/main.py
+++ kakarot_scripts/deployment/main.py
@@ -7,10 +7,6 @@ from kakarot_scripts.constants import EVM_ADDRESS, L1_RPC_PROVIDER, NETWORK, Net
 from kakarot_scripts.deployment.dualvm_token_deployments import deploy_dualvm_tokens
 from kakarot_scripts.deployment.evm_deployments import deploy_evm_contracts
 from kakarot_scripts.deployment.kakarot_deployment import deploy_or_upgrade_kakarot
-from kakarot_scripts.deployment.messaging_deployments import (
-    deploy_l1_messaging_contracts,
-    deploy_l2_messaging_contracts,
-)
 from kakarot_scripts.deployment.pre_eip155_deployments import (
     deploy_pre_eip155_contracts,
     deploy_pre_eip155_senders,
@@ -68,10 +64,6 @@ async def main():
     remove_lazy_account(account.address)
     # Needs whitelist tx to be executed first
     await deploy_pre_eip155_contracts()
-
-    # %% Messaging
-    await deploy_l1_messaging_contracts()
-    await deploy_l2_messaging_contracts()
 
     # %% Tear down
     coinbase_address = (await call("kakarot", "get_coinbase")).coinbase
